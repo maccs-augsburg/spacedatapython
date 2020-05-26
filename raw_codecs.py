f8f8970bf4d686ffd8237f6bf5eb6ddbe9713277
Erik S. Steinmetz <erik@steinmetz.org>
2020-05-26 11:15:04 -0500

    Formatted doc comment, changed tabs to spaces in code.

--- raw_codecs.py
+++ raw_codecs.py
@@ -6,19 +6,26 @@
 #  2020 May  -- Created  -- Erik Steinmetz
 #
 
-def decode(bytes):
+def decode(bytes) :
     """ 
-    Converts raw bytes into a integer value.
+    Converts 24 bit integer into a integer value.
     
     Parameters
-    bytes: 3 byte array (24 bits) of 2's complement value
+    ----------
+    bytes : 
+        A 3 byte array (24 bits) of 2's complement value
+        
     Returns
+    -------
     int
         The python integer value represented by the 24 bits.
     """
-    value = bytes[0]*65536+bytes[1]*256+bytes[2]
-	if value>= 2**23:
-		return value - 2**24
-	else:
-		return value
+    
+    value = bytes[0] * 65536 + bytes[1] * 256 + bytes[2]
+    if value >= 2**23 :
+        return value - 2**24
+    else :
+        return value
+
+
 
