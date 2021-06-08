ae2f1a008f4ad3198402759b500f33155823a484
Ted Strombeck <ted.strombeck1@gmail.com>
2021-06-08 09:43:09 -0500

    Added initial documentation for plotter GUI

---
+++ PlotterGUIStrombeck.py
@@ -0,0 +1,13 @@
+# PlotterGUIStrombeck.py
+#
+# 2021 June - Created - Ted Strombeck
+#
+
+""" Python Plotter GUI
+
+GUI that allows for an easier interaction with our plotter program to test
+and use MACCS data files which graphs the time-stamped x, y, and z values
+on its' own plot.
+
+"""
+
