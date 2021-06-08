d1beba3517599e11edfd19dc8714b8de15b5a576
Ted Strombeck <ted.strombeck1@gmail.com>
2021-06-08 09:51:31 -0500

    added grid initialization to mainframe object

--- PlotterGUIStrombeck.py
+++ PlotterGUIStrombeck.py
@@ -19,5 +19,6 @@ from tkinter import ttk
 root = Tk()
 root.title("Plot input")
 mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
