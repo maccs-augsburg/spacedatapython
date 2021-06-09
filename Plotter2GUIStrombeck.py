81ef020340da18e645d5e7323abca7854334a6f0
Ted Strombeck <ted.strombeck1@gmail.com>
2021-06-09 14:14:02 -0500

    Creating and Initial documentation for Plotter2GUIStrombeck.py

---
+++ Plotter2GUIStrombeck.py
@@ -0,0 +1,18 @@
+# Plotter2GUIStrombeck.py
+#
+# 2021 June - Created - Ted Strombeck
+#
+
+""" Python Plotter GUI - 2nd Version
+
+GUI that allows for an easier interaction with our plotter program to test
+and use MACCS data files which graphs the time-stamped x, y, and z values
+on its' own plot.
+
+"""
+
+# tkinter imports
+from tkinter import *
+from tkinter import ttk
+
+### main ###
