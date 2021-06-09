268796ad3da2d1bfe0c7b03bf4af99db8d3e0eb4
Ted Strombeck <ted.strombeck1@gmail.com>
2021-06-09 14:40:49 -0500

    Updated spacing for plot min and plot max, removed the multiple station file and file format options

--- PlotterGUIStrombeck.py
+++ PlotterGUIStrombeck.py
@@ -9,6 +9,8 @@ GUI that allows for an easier interaction with our plotter program to test
 and use MACCS data files which graphs the time-stamped x, y, and z values
 on its' own plot.
 
+This GUI uses a radiobutton format for selection of file type
+
 """
 
 # tkinter imports
@@ -45,6 +47,28 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+### Label section ###
+# Yearday section
+ttk.Label(mainframe, text="Yearday:").grid(column=1, row=1, sticky=W)
+
+# Start time section
+ttk.Label(mainframe, text="Start Hour:").grid(column=1, row=2, sticky=W)
+ttk.Label(mainframe, text="Start Minute:").grid(column=2, row=2, sticky=W)
+
+# End time section
+ttk.Label(mainframe, text="End Hour:").grid(column=1, row=3, sticky=W)
+ttk.Label(mainframe, text="End Minute:").grid(column=2, row=3, sticky=W)
+
+# Plot min and max section
+ttk.Label(mainframe, text="Plot Min (leave at 0 for default):").grid(column=1, row=4, sticky=W)
+ttk.Label(mainframe, text="Plot Max (leave at 0 for default):").grid(column=1, row=5, sticky=W)
+
+# Station file section
+ttk.Label(mainframe, text="Station file:").grid(column=1, row=6, sticky=W)
+
+# File format section
+ttk.Label(mainframe, text="File format (pick from list below)").grid(column=1, row=7, sticky=W)
+
 ### Entry section ###
 # Yearday entries
 yearday = StringVar() ## storing as a string for now, might change to int later
@@ -58,7 +82,7 @@ startHour_entry.grid(column=1, row=2)
 
 startMinute = IntVar()
 startMinute_entry = ttk.Entry(mainframe, width=3, textvariable=startMinute)
-startMinute_entry.grid(column=2, row=2)
+startMinute_entry.grid(column=3, row=2, sticky=W)
 
 endHour = IntVar()
 endHour_entry = ttk.Entry(mainframe, width=3, textvariable=endHour)
@@ -66,70 +90,40 @@ endHour_entry.grid(column=1, row=3)
 
 endMinute = IntVar()
 endMinute_entry = ttk.Entry(mainframe, width=3, textvariable=endMinute)
-endMinute_entry.grid(column=2, row=3)
+endMinute_entry.grid(column=3, row=3, sticky=W)
 
 # Plot min and Plot max entries
-    # TODO: add plot min and plot max entry boxes
+plotMin = IntVar()
+plotMin_Entry = ttk.Entry(mainframe, width=3, textvariable=plotMin)
+plotMin_Entry.grid(column=2, row=4, sticky=W)
+plotMax = IntVar()
+plotMax_Entry = ttk.Entry(mainframe, width=3, textvariable=plotMax)
+plotMax_Entry.grid(column=2, row=5, sticky=W)
 
 # Station file entries
 stationfile1 = StringVar()
 stationfile1_entry = ttk.Entry(mainframe, width=3, textvariable=stationfile1)
-stationfile1_entry.grid(column=1, row=5)
-
-stationfile2 = StringVar()
-stationfile2_entry = ttk.Entry(mainframe, width=3, textvariable=stationfile2)
-stationfile2_entry.grid(column=2, row=5)
-
-stationfile3 = StringVar()
-stationfile3_entry = ttk.Entry(mainframe, width=3, textvariable=stationfile3)
-stationfile3_entry.grid(column=4, row=5)
+stationfile1_entry.grid(column=1, row=6)
 
 ### Button section ###
 # Management buttons section
-ok_button = ttk.Button(mainframe, text="OK", command=calculate).grid(column=1, row = 14, sticky=W)
-cancel_button = ttk.Button(mainframe, text="Cancel", command=cancel).grid(column=2, row=14, sticky=W)
+ok_button = ttk.Button(mainframe, text="OK", command=calculate).grid(column=1, row = 15, sticky=W)
+cancel_button = ttk.Button(mainframe, text="Cancel", command=cancel).grid(column=1, row=15, sticky=E)
 # Listbox buttons section
 #listbox1 = Listbox(mainframe, height=10, width=15, bg="grey", font="Helvetica")
-#listbox2 = Listbox(mainframe, height=10, width=15, bg="grey", font="Helvetica")
-#listbox3 = Listbox(mainframe, height=10, width=15, bg="grey", font="Helvetica")
     #selectmode="multiple"
 #fillList(listbox1)
-#fillList(listbox2)
-#fillList(listbox3)
 #listbox1.grid(column=1, row=7, sticky=(W,E))
-#listbox2.grid(column=2, row=7, sticky=(W,E))
-#listbox3.grid(column=3, row=7, sticky=(W,E))
 
 # Radiobutton section
 fileSelection = StringVar()
-rb1 = Radiobutton(mainframe, text="CDAWEB", value=1, variable=fileSelection).grid(column=1, row=7)
-rb2 = Radiobutton(mainframe, text="IAGA2000", value=2, variable=fileSelection).grid(column=1, row=8)
-rb3 = Radiobutton(mainframe, text="IAGA2002", value=3, variable=fileSelection).grid(column=1, row=9)
-rb4 = Radiobutton(mainframe, text="Augsburg", value=4, variable=fileSelection).grid(column=1, row=10)
-rb5 = Radiobutton(mainframe, text="AAL-PIP", value=5, variable=fileSelection).grid(column=1, row=11)
-rb6 = Radiobutton(mainframe, text="SPole", value=6, variable=fileSelection).grid(column=1, row=12)
-rb7 = Radiobutton(mainframe, text="other", value=7, variable=fileSelection).grid(column=1, row=13)
-
-### Label section ###
-# Yearday section
-ttk.Label(mainframe, text="Yearday:").grid(column=1, row=1, sticky=W)
-# Start time section
-ttk.Label(mainframe, text="Start Hour:").grid(column=1, row=2, sticky=W)
-ttk.Label(mainframe, text="Start Minute:").grid(column=2, row=2, sticky=W)
-# End time section
-ttk.Label(mainframe, text="End Hour:").grid(column=1, row=3, sticky=W)
-ttk.Label(mainframe, text="End Minute:").grid(column=2, row=3, sticky=W)
-# Plot min and max section
-ttk.Label(mainframe, text="Plot Min (leave at 0 for default):").grid(column=1, row=4, sticky=W)
-ttk.Label(mainframe, text="Plot Max (leave at 0 for default):").grid(column=2, row=4, sticky=W)
-# Station file section
-ttk.Label(mainframe, text="Station file 1:").grid(column=1, row=5, sticky=W)
-ttk.Label(mainframe, text="Station file 2:").grid(column=2, row=5, sticky=W)
-ttk.Label(mainframe, text="Station file 3:").grid(column=3, row=5, sticky=W)
-# File format section
-ttk.Label(mainframe, text="File 1 format:").grid(column=1, row=6, sticky=W)
-ttk.Label(mainframe, text="File 2 format:").grid(column=2, row=6, sticky=W)
-ttk.Label(mainframe, text="File 3 format:").grid(column=3, row=6, sticky=W)
+rb1 = Radiobutton(mainframe, text="CDAWEB", value=1, variable=fileSelection).grid(column=1, row=8)
+rb2 = Radiobutton(mainframe, text="IAGA2000", value=2, variable=fileSelection).grid(column=1, row=9)
+rb3 = Radiobutton(mainframe, text="IAGA2002", value=3, variable=fileSelection).grid(column=1, row=10)
+rb4 = Radiobutton(mainframe, text="Augsburg", value=4, variable=fileSelection).grid(column=1, row=11)
+rb5 = Radiobutton(mainframe, text="AAL-PIP", value=5, variable=fileSelection).grid(column=1, row=12)
+rb6 = Radiobutton(mainframe, text="SPole", value=6, variable=fileSelection).grid(column=1, row=13)
+rb7 = Radiobutton(mainframe, text="other", value=7, variable=fileSelection).grid(column=1, row=14)
 
 ### Main ###
 # child formatting in mainframe
