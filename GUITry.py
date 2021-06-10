e1618c4b8ed8b5773ca79c1f46e4e029863a73b1
Annabelle Arns <annabellearns269@gmail.com>
2021-06-10 17:31:17 -0500

    GUI shows up again, started adding spacing to the GUI

--- GUITry.py
+++ GUITry.py
@@ -23,26 +23,26 @@ start_Hour_entry.grid(column = 1, row = 1, sticky = (W, E))
 
 start_Minute =StringVar()
 start_Minute_entry = ttk.Entry(mainframe, width = 5, textvariable = "Start Minute")
-start_Minute_entry.grid(column = 1, row = 2, sticky = (W, E))
+start_Minute_entry.grid(column = 1, row = 4, sticky = (W, E))
 
 
 start_Second =StringVar()
 start_Second_entry = ttk.Entry(mainframe, width = 5, textvariable = "Start Second")
-start_Second_entry.grid(column = 2, row = 1, sticky = (W, E))
+start_Second_entry.grid(column = 3, row = 1, sticky = (W, E))
 
 
 end_Hour = StringVar()
 end_Hour_entry = ttk.Entry(mainframe, width = 5, textvariable = "End Hour")
-end_Hour_entry.grid(column = 3, row = 1, sticky = (W,E))
+end_Hour_entry.grid(column = 5, row = 1, sticky = (W,E))
 
 
 end_Minute = StringVar()
 end_Minute_entry = ttk.Entry(mainframe, width = 5, textvariable = "End Minute")
-end_Minute_entry.grid(column = 3, row = 2, sticky = (W,E))
+end_Minute_entry.grid(column = 5, row = 2, sticky = (W,E))
 
 end_Second =StringVar()
 end_Second_entry = ttk.Entry(mainframe, width = 5, textvariable = "End Second")
-end_Second_entry.grid(column = 4, row = 1, sticky = (W,E))
+end_Second_entry.grid(column = 8, row = 1, sticky = (W,E))
 
 #Creating the Radio Buttons Widgets
 
