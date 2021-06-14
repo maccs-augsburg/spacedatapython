c7d975e48bab7d139284230a2845eead89ff8774
Annabelle Arns <annabellearns269@gmail.com>
2021-06-14 14:49:58 -0500

    Added the imports, the main application, and the content frame.

--- LinestackGUI.py
+++ LinestackGUI.py
@@ -1,2 +1,22 @@
 #Linestack GUI
 #Annabelle Arns 
+
+from tkinter import *
+from tkinter import ttk
+
+#Main Application Frame
+root = Tk()
+root.title = ("Plot Input")
+
+#Content Frame
+mainframe = ttk.Frame(root, padding = "3 3 12 12")
+mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
+root.columnconfigure(0, weight = 1)
+root.rowconfigure(0, weight = 1)
+
+
+
+
+#Creating the Entry Widgets
+
+
