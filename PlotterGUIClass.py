2dd80880924796863284fb92b596b1c0b683a85c
Ted Strombeck <ted.strombeck1@gmail.com>
2021-06-16 11:16:46 -0500

    Created GUI class file to be able to separate PlotterGUIStrombeck.py into different functions

---
+++ PlotterGUIClass.py
@@ -0,0 +1,20 @@
+# PlotterGUIClass.py
+#
+# 2021 June - Created - Ted Strombeck
+
+""" Python Plotter GUI Class
+
+GUI class that allows for an easier interaction with our plotter program
+using various functions to access and use MACCS data files which graphs
+the time-stamped x, y, and z values on its' own plot.
+
+"""
+
+# tkinter imports
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+# Python 3 imports
+import sys
+import datetime
