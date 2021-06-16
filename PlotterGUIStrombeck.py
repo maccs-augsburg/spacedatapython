7067b5914195b0154b8f9315ede1c2d30649fafd
Ted Strombeck <ted.strombeck1@gmail.com>
2021-06-16 12:19:06 -0500

    Added main function to file instead of having the code on the far left side

--- PlotterGUIStrombeck.py
+++ PlotterGUIStrombeck.py
@@ -19,7 +19,7 @@ This GUI uses a radiobutton format for selection of file type
 #   add seconds as input for time as well ---------------------------------------------------------> done
 #   add initial tests to file types and file save as to see which values are selected -------------> done
 #   add error messages for bad inputs -------------------------------------------------------------> done
-#   Create datetime objects of inputted times -----------------------------------------------------> working on
+#   Create datetime objects of inputted times -----------------------------------------------------> done
 #   documentation --------------------------------------------------------------------------------->
 #   main function --------------------------------------------------------------------------------->
 #   general gui function -------------------------------------------------------------------------->
@@ -27,6 +27,7 @@ This GUI uses a radiobutton format for selection of file type
 #   change calculate function name to something like display plot --------------------------------->
 #   view box around radio buttons ----------------------------------------------------------------->
 #   go through and fully comment code ------------------------------------------------------------->
+#   variable names change -------------------------------------------------------------------------> working on
 #--------------------------------------------------------------------------------------------------------------
 
 # tkinter imports
@@ -186,6 +187,131 @@ def cancel(*args):
     root.destroy() # Exiting without running any code after
     #global.root
     #root.quit() # Exiting with running code after
+    
+
+def main(root, mainframe):
+
+    ### Label section ###
+    # Yearday section
+    ttk.Label(mainframe, text="Yearday:").grid(column=1, row=1, sticky=W)
+
+    # Start time section
+    ttk.Label(mainframe, text="Start Hour:").grid(column=1, row=2, sticky=W)
+    ttk.Label(mainframe, text="Start Minute:").grid(column=2, row=2, sticky=W)
+    ttk.Label(mainframe, text="StartSecond:").grid(column=4, row=2, sticky=W)
+
+    # End time section
+    ttk.Label(mainframe, text="End Hour:").grid(column=1, row=3, sticky=W)
+    ttk.Label(mainframe, text="End Minute:").grid(column=2, row=3, sticky=W)
+    ttk.Label(mainframe, text="End Second:").grid(column=4, row=3, sticky=W)
+
+    # Plot min and max section
+    ttk.Label(mainframe, text="Plot Min (leave at 0 for default):").grid(column=1, row=4, sticky=W)
+    ttk.Label(mainframe, text="Plot Max (leave at 0 for default):").grid(column=1, row=5, sticky=W)
+
+    # Station file section
+    ttk.Label(mainframe, text="Station code (3-4):").grid(column=1, row=6, sticky=W)
+
+    # File format section
+    ttk.Label(mainframe, text="Format of file to Open (pick from list below)").grid(column=1, row=7, sticky=W)
+
+    # File to save as section
+    ttk.Label(mainframe, text="Save file as (pick from list below)").grid(column=1, row=15, sticky=W)
+
+    ### Entry section ###
+    # Yearday
+    global yearday, yearday_entry
+    yearday = StringVar() ## storing as a string for now, might change to int later
+    yearday_entry = ttk.Entry(mainframe, width=6, textvariable=yearday) # setting a variable with the Entry box format
+    yearday_entry.grid(column=1, row=1) # selecting which column and row to place said variable
+
+    # Start Hour
+    global startHour, startHour_entry
+    startHour = IntVar()
+    startHour.set(0)
+    startHour_entry = ttk.Entry(mainframe, width=3, textvariable=startHour)
+    startHour_entry.grid(column=1, row=2)
+
+    # Start Minute
+    global startMinute, startMinute_entry
+    startMinute = IntVar()
+    startMinute_entry = ttk.Entry(mainframe, width=3, textvariable=startMinute)
+    startMinute_entry.grid(column=3, row=2, sticky=W)
+
+    # Start Second
+    global startSecond, startSecond_entry
+    startSecond = IntVar()
+    startSecond_entry = ttk.Entry(mainframe, width=3, textvariable=startSecond)
+    startSecond_entry.grid(column=5, row=2, sticky=W)
+
+    # End Hour
+    global endHour, endHour_entry
+    endHour = IntVar()
+    endHour.set(23)
+    endHour_entry = ttk.Entry(mainframe, width=3, textvariable=endHour)
+    endHour_entry.grid(column=1, row=3)
+
+    # End Minute
+    global endMinute, endMinute_entry
+    endMinute = IntVar()
+    endMinute.set(59)
+    endMinute_entry = ttk.Entry(mainframe, width=3, textvariable=endMinute)
+    endMinute_entry.grid(column=3, row=3, sticky=W)
+
+    # End Second
+    global endSecond, endSecond_entry
+    endSecond = IntVar()
+    endSecond.set(59)
+    endSecond_entry = ttk.Entry(mainframe, width=3, textvariable=endSecond)
+    endSecond_entry.grid(column=5, row=3, sticky=W)
+
+    # Plot min and Plot max entries
+    global plotMin, plotMin_entry, plotMax, plotMax_entry
+    plotMin = IntVar()
+    plotMin_entry = ttk.Entry(mainframe, width=3, textvariable=plotMin)
+    plotMin_entry.grid(column=2, row=4, sticky=W)
+    plotMax = IntVar()
+    plotMax_entry = ttk.Entry(mainframe, width=3, textvariable=plotMax)
+    plotMax_entry.grid(column=2, row=5, sticky=W)
+
+    # Station file entries
+    global stationcode1, stationcode1_entry
+    stationcode1 = StringVar()
+    stationcode1_entry = ttk.Entry(mainframe, width=4, textvariable=stationcode1)
+    stationcode1_entry.grid(column=1, row=6)
+
+    ### Button section ###
+    # Management buttons section
+    ok_button = ttk.Button(mainframe, text="OK", command=calculate).grid(column=2, row = 19, sticky=W)
+    cancel_button = ttk.Button(mainframe, text="Cancel", command=cancel).grid(column=3, row=19, sticky=E)
+
+    # Radiobutton section
+    # file selection of type of file to open
+    global fileSelection, rb1, rb2, rb3, rb4, rb7
+    fileSelection = StringVar()
+    rb1 = Radiobutton(mainframe, text="CDAWEB -- Not working", value=1, variable=fileSelection).grid(column=1, row=8, sticky=W)
+    rb2 = Radiobutton(mainframe, text="IAGA2000 -- Not working ", value=2, variable=fileSelection).grid(column=1, row=9, sticky=W)
+    rb3 = Radiobutton(mainframe, text="IAGA2002 -- Not working", value=3, variable=fileSelection).grid(column=1, row=10, sticky=W)
+    rb4 = Radiobutton(mainframe, text="Raw 2hz file", value=4, variable=fileSelection).grid(column=1, row=11, sticky=W)
+    #rb5 = Radiobutton(mainframe, text="AAL-PIP", value=5, variable=fileSelection).grid(column=1, row=12, sticky=W)
+    #rb6 = Radiobutton(mainframe, text="SPole", value=6, variable=fileSelection).grid(column=1, row=13, sticky=W)
+    rb7 = Radiobutton(mainframe, text="other -- Not working", value=7, variable=fileSelection).grid(column=1, row=14, sticky=W)
+
+    # file selection of type of file to save it as
+    global fileToSaveAs, rb8, rb9, rb10
+    fileToSaveAs = StringVar()
+    rb8 = Radiobutton(mainframe, text="pdf", value=8, variable=fileToSaveAs).grid(column=1, row=16, sticky=W)
+    rb9 = Radiobutton(mainframe, text="png", value=9, variable=fileToSaveAs).grid(column=1, row=17, sticky=W)
+    rb10 = Radiobutton(mainframe, text="Do not save", value=10, variable=fileToSaveAs).grid(column=1, row=18, sticky=W)
+
+    ### Main ###
+    # child formatting in mainframe
+    for child in mainframe.winfo_children(): 
+        child.grid_configure(padx=5, pady=5)
+
+    yearday_entry.focus() # starting spot for tab control
+    root.bind("<Return>", calculate) # returns the calculate funciton when called
+
 
 ### Setting up GUI object ###
 root = Tk()
@@ -195,114 +321,5 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-### Label section ###
-# Yearday section
-ttk.Label(mainframe, text="Yearday:").grid(column=1, row=1, sticky=W)
-
-# Start time section
-ttk.Label(mainframe, text="Start Hour:").grid(column=1, row=2, sticky=W)
-ttk.Label(mainframe, text="Start Minute:").grid(column=2, row=2, sticky=W)
-ttk.Label(mainframe, text="StartSecond:").grid(column=4, row=2, sticky=W)
-
-# End time section
-ttk.Label(mainframe, text="End Hour:").grid(column=1, row=3, sticky=W)
-ttk.Label(mainframe, text="End Minute:").grid(column=2, row=3, sticky=W)
-ttk.Label(mainframe, text="End Second:").grid(column=4, row=3, sticky=W)
-
-# Plot min and max section
-ttk.Label(mainframe, text="Plot Min (leave at 0 for default):").grid(column=1, row=4, sticky=W)
-ttk.Label(mainframe, text="Plot Max (leave at 0 for default):").grid(column=1, row=5, sticky=W)
-
-# Station file section
-ttk.Label(mainframe, text="Station code (3-4):").grid(column=1, row=6, sticky=W)
-
-# File format section
-ttk.Label(mainframe, text="Format of file to Open (pick from list below)").grid(column=1, row=7, sticky=W)
-
-# File to save as section
-ttk.Label(mainframe, text="Save file as (pick from list below)").grid(column=1, row=15, sticky=W)
-
-### Entry section ###
-# Yearday
-yearday = StringVar() ## storing as a string for now, might change to int later
-yearday_entry = ttk.Entry(mainframe, width=6, textvariable=yearday) # setting a variable with the Entry box format
-yearday_entry.grid(column=1, row=1) # selecting which column and row to place said variable
-
-# Start Hour
-startHour = IntVar()
-startHour.set(0)
-startHour_entry = ttk.Entry(mainframe, width=3, textvariable=startHour)
-startHour_entry.grid(column=1, row=2)
-
-# Start Minute
-startMinute = IntVar()
-startMinute_entry = ttk.Entry(mainframe, width=3, textvariable=startMinute)
-startMinute_entry.grid(column=3, row=2, sticky=W)
-
-# Start Second
-startSecond = IntVar()
-startSecond_entry = ttk.Entry(mainframe, width=3, textvariable=startSecond)
-startSecond_entry.grid(column=5, row=2, sticky=W)
-
-# End Hour
-endHour = IntVar()
-endHour.set(23)
-endHour_entry = ttk.Entry(mainframe, width=3, textvariable=endHour)
-endHour_entry.grid(column=1, row=3)
-
-# End Minute
-endMinute = IntVar()
-endMinute.set(59)
-endMinute_entry = ttk.Entry(mainframe, width=3, textvariable=endMinute)
-endMinute_entry.grid(column=3, row=3, sticky=W)
-
-# End Second
-endSecond = IntVar()
-endSecond.set(59)
-endSecond_entry = ttk.Entry(mainframe, width=3, textvariable=endSecond)
-endSecond_entry.grid(column=5, row=3, sticky=W)
-
-# Plot min and Plot max entries
-plotMin = IntVar()
-plotMin_entry = ttk.Entry(mainframe, width=3, textvariable=plotMin)
-plotMin_entry.grid(column=2, row=4, sticky=W)
-plotMax = IntVar()
-plotMax_entry = ttk.Entry(mainframe, width=3, textvariable=plotMax)
-plotMax_entry.grid(column=2, row=5, sticky=W)
-
-# Station file entries
-stationcode1 = StringVar()
-stationcode1_entry = ttk.Entry(mainframe, width=4, textvariable=stationcode1)
-stationcode1_entry.grid(column=1, row=6)
-
-### Button section ###
-# Management buttons section
-ok_button = ttk.Button(mainframe, text="OK", command=calculate).grid(column=2, row = 19, sticky=W)
-cancel_button = ttk.Button(mainframe, text="Cancel", command=cancel).grid(column=3, row=19, sticky=E)
-
-# Radiobutton section
-# file selection of type of file to open
-fileSelection = StringVar()
-rb1 = Radiobutton(mainframe, text="CDAWEB -- Not working", value=1, variable=fileSelection).grid(column=1, row=8, sticky=W)
-rb2 = Radiobutton(mainframe, text="IAGA2000 -- Not working ", value=2, variable=fileSelection).grid(column=1, row=9, sticky=W)
-rb3 = Radiobutton(mainframe, text="IAGA2002 -- Not working", value=3, variable=fileSelection).grid(column=1, row=10, sticky=W)
-rb4 = Radiobutton(mainframe, text="Raw 2hz file", value=4, variable=fileSelection).grid(column=1, row=11, sticky=W)
-#rb5 = Radiobutton(mainframe, text="AAL-PIP", value=5, variable=fileSelection).grid(column=1, row=12, sticky=W)
-#rb6 = Radiobutton(mainframe, text="SPole", value=6, variable=fileSelection).grid(column=1, row=13, sticky=W)
-rb7 = Radiobutton(mainframe, text="other -- Not working", value=7, variable=fileSelection).grid(column=1, row=14, sticky=W)
-
-# file selection of type of file to save it as
-fileToSaveAs = StringVar()
-rb8 = Radiobutton(mainframe, text="pdf", value=8, variable=fileToSaveAs).grid(column=1, row=16, sticky=W)
-rb9 = Radiobutton(mainframe, text="png", value=9, variable=fileToSaveAs).grid(column=1, row=17, sticky=W)
-rb10 = Radiobutton(mainframe, text="Do not save", value=10, variable=fileToSaveAs).grid(column=1, row=18, sticky=W)
-
-### Main ###
-# child formatting in mainframe
-for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
-
-yearday_entry.focus() # starting spot for tab control
-root.bind("<Return>", calculate) # returns the calculate funciton when called
-
+main(root, mainframe)
 root.mainloop() # root loop running
