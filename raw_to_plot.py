56c58c9abe59e920f94d09ccc10245f6ceed91f8
Ted Strombeck <ted.strombeck1@gmail.com>
2021-06-23 17:14:38 -0500

    Updated naming convention for varaibles in all functions

--- raw_to_plot.py
+++ raw_to_plot.py
@@ -31,7 +31,7 @@ import station_names
 import matplotlib.pyplot as plt
 from matplotlib.ticker import(MultipleLocator, AutoMinorLocator)
 
-def find_max_differences_of_three(xArr, yArr, zArr) :
+def find_max_differences_of_three(, y_arr, z_arr) :
     """
     Finds the difference between the minimum value and maximum values in the list
         for the x, y, and z lists
@@ -39,9 +39,9 @@ def find_max_differences_of_three(xArr, yArr, zArr) :
     Parameters
     ----------
     List
-        xArr: a list of x values to find the difference in
-        yArr: a list of y values to find the difference in
-        zArr: a list of z valeus to find the difference in
+        x_arr: a list of x values to find the difference in
+        y_arr: a list of y values to find the difference in
+        z_arr: a list of z valeus to find the difference in
 
     Returns
     -------
@@ -50,9 +50,9 @@ def find_max_differences_of_three(xArr, yArr, zArr) :
     """
 
     # getting the differences for the arrays
-    x_difference = find_differences_in_list(xArr)
-    y_difference = find_differences_in_list(yArr)
-    z_difference = find_differences_in_list(zArr)
+    x_difference = find_differences_in_list(x_arr)
+    y_difference = find_differences_in_list(y_arr)
+    z_difference = find_differences_in_list(z_arr)
 
     # setting the max to be the x_difference initially
     max_difference = x_difference
@@ -101,7 +101,7 @@ def find_differences_in_list (arr) :
     difference = maximum_value - minimum_value
     return difference
 
-def create_Arrays (raw_record, stime, etime) :
+def create_arrays (raw_record, stime, etime) :
     """ Creates x, y, z lists based on the 2 Hz raw data file.
     
     Places x, y, and z values into their specific lists.
@@ -119,20 +119,20 @@ def create_Arrays (raw_record, stime, etime) :
     Returns
     -------
     List
-        xArr: a list of x Values from our 2 Hz file
+        x_arr: a list of x Values from our 2 Hz file
     List
-        yArr: a list of y Values from our 2 Hz file
+        y_arr: a list of y Values from our 2 Hz file
     List
-        zArr: a list of z Values from our 2 Hz file
+        z_arr: a list of z Values from our 2 Hz file
     List
-        timeArr: a list of time Values from our 2 Hz file
+        time_arr: a list of time Values from our 2 Hz file
     """
 
     # Lists to hold data and return at end of function
-    xArr = []	    #x plot point storage
-    yArr = []	    #y plot point storage
-    zArr = []       #z plot point storage
-    timeArr = []    #time plot point storage
+    x_arr = []	    #x plot point storage
+    y_arr = []	    #y plot point storage
+    z_arr = []       #z plot point storage
+    time_arr = []    #time plot point storage
 
     #Initial while loop condition
     while True :
@@ -152,29 +152,29 @@ def create_Arrays (raw_record, stime, etime) :
             second = one_record[6]
 
             # converting it into hours for the time array 
-            timeInHours = hour +  (minute / 60) + (second / 3600)
-            timeArr.append(timeInHours) # Converted to hours
+            time_in_hours = hour +  (minute / 60) + (second / 3600)
+            time_arr.append(time_in_hours) # Converted to hours
 
             # getting the x values
             x1 = decode( one_record[18:21]) / 40.0
-            xArr.append(x1)
+            x_arr.append(x1)
 
             # getting the y values
             y1 = decode( one_record[21:24]) / 40.0
-            yArr.append(y1) 
+            y_arr.append(y1) 
 
             # getting the z values
             z1 = decode( one_record[24:27]) / 40.0
-            zArr.append(z1)
+            z_arr.append(z1)
 
         # if current time is greater than or equal to the ending time then we stop
         if current_time >= etime :
             break
 
     # returning the 4 lists
-    return xArr, yArr, zArr, timeArr
+    return x_arr, y_arr, z_arr, time_arr
 
-def plot_Arrays(xArr, yArr, zArr, timeArr, filename, stime, etime, fileOption) :
+def plot_arrays(x_arr, y_arr, z_arr, time_arr, filename, stime, etime, file_option) :
     """ Places x, y, z arrays on a plot.
 
     Places x, y, and z arrays which contain values from the
@@ -182,13 +182,13 @@ def plot_Arrays(xArr, yArr, zArr, timeArr, filename, stime, etime, fileOption) :
 
     Parameters
     ----------
-    xArr:
+    x_arr:
         List of x values from a 2 Hz raw data file.
-    yArr:
+    y_arr:
         List of y values from a 2 Hz raw data file.
-    zArr:
+    z_arr:
         List of z values from a 2 Hz raw data file.
-    timeArr:
+    time_arr:
         List of time values from a 2Hz raw data file.
     filename:
         Name of the 2Hz raw data file.
@@ -196,44 +196,44 @@ def plot_Arrays(xArr, yArr, zArr, timeArr, filename, stime, etime, fileOption) :
         The datetime.time object from which to begin
     etime:
         The datetime.time object at which to end
-    fileOption:
+    file_option:
         String to indicate what type of file to save (PDF or PNG)
     """
 
     ### splitting up the file name
     station = filename[0:2] # Two letter abbreviation of station
-    stationName = station_names.find_full_name(station) # Getting the station name
-    yearDayValue = filename[2:7] # Year: (first two digits) and day of year: (last 3 digits)
-    yearValue = yearDayValue[0:2] # The last 2 digits of the year
-    dayValue = yearDayValue[2:] # The 3 digits corresponding with the day of the year
+    station_name = station_names.find_full_name(station) # Getting the station name
+    year_day_value = filename[2:7] # Year: (first two digits) and day of year: (last 3 digits)
+    year_value = year_day_value[0:2] # The last 2 digits of the year
+    day_value = year_day_value[2:] # The 3 digits corresponding with the day of the year
 
-    if((int)(yearValue) > 50): # Not sure what the cutoff should be, just defaulted to 50 to start with
-        yearValue = "19" + yearValue
+    if((int)(year_value) > 50): # Not sure what the cutoff should be, just defaulted to 50 to start with
+        year_value = "19" + year_value
     else:
-        yearValue = "20" + yearValue
+        year_value = "20" + year_value
 
     # Converting the date and setting it up
-    date = datetime.datetime.strptime(yearValue + "-" + dayValue, "%Y-%j").strftime("%m-%d-%Y") 
+    date = datetime.datetime.strptime(year_value + "-" + day_value, "%Y-%j").strftime("%m-%d-%Y") 
     
 
     ### hour list and determining which one to use
-    defaultHoursArr = [1,3,5,7,9,11,13,15,17,19,21,23] # default graph list
-    hoursArr = [] # list to use for custom times
-    currentTime = stime.hour # setting the time to the start
-    defaultHoursFlag = False # using a flag to better optimize operations
+    default_hours_arr = [1,3,5,7,9,11,13,15,17,19,21,23] # default graph list
+    hours_arr = [] # list to use for custom times
+    current_time = stime.hour # setting the time to the start
+    default_hours_flag = False # using a flag to better optimize operations
 
     # setting the flag to true if the stime and etimes are the full 24 hours
     if (stime == datetime.time.fromisoformat( "00:00:00") and etime == datetime.time.fromisoformat('23:59:59')):
-        defaultHoursFlag = True
+        default_hours_flag = True
 
     # Create a loop that fills out an list with odd numbers from start time to end time
-    if not defaultHoursFlag:
+    if not default_hours_flag:
         for i in range(stime.hour, etime.hour, 1): #intial for loop to iterate throughout the given times
 
             # only adding the odd numbers to the list
-            if(currentTime % 2 != 0):
-                hoursArr.append(currentTime) # adding the odd numbers to the list
-            currentTime += 1 # incrementing currentTime
+            if(current_time % 2 != 0):
+                hours_arr.append(current_time) # adding the odd numbers to the list
+            current_time += 1 # incrementing current_time
 
     ### figure settings
     fig = plt.figure(figsize=(12, 7)) #12, 7, dictates width, height
@@ -246,8 +246,8 @@ def plot_Arrays(xArr, yArr, zArr, timeArr, filename, stime, etime, fileOption) :
                         # 3 dictates the range (row), allowing 3 graphs
                         # 1 indicates columns, more than 1 for matrices for example
                         # 1 indicates which subplot out of 3 to work on
-    plt.plot(timeArr,xArr, linewidth=0.25) # this was plt.scatter, we used plt.plot for a line graph
-    plt.title("Geomagnetic Bx By Bz of " + stationName + "          YEARDAY: " + yearDayValue + "            DATE: " + date) # setting up the title and yearday
+    plt.plot(time_arr,x_arr, linewidth=0.25) # this was plt.scatter, we used plt.plot for a line graph
+    plt.title("Geomagnetic Bx By Bz of " + station_name + "          YEARDAY: " + year_day_value + "            DATE: " + date) # setting up the title and yearday
     plt.ylabel('Bx')	# side label
     plt.gca().axes.xaxis.set_ticklabels([]) # removing x axis numbers
     plt.autoscale(enable=True, axis='x', tight=True) # adjusting x axis scaling
@@ -255,14 +255,14 @@ def plot_Arrays(xArr, yArr, zArr, timeArr, filename, stime, etime, fileOption) :
     plt.gca().tick_params(left=True, right=True) # Putting ticks on both sides of y axis
     plt.gca().tick_params(axis='x', direction='in') # x axis ticks inverted
     plt.gca().tick_params(axis='y', direction='in') # y axis ticks inverted
-    if (defaultHoursFlag):
-        plt.xticks(defaultHoursArr) # setting the xaxis time ticks to 1 to 24 hours
+    if (default_hours_flag):
+        plt.xticks(default_hours_arr) # setting the xaxis time ticks to 1 to 24 hours
     else:
-        plt.xticks(hoursArr) # setting the xaxis time ticks to custom values
+        plt.xticks(hours_arr) # setting the xaxis time ticks to custom values
 
     ### Now build the second plot, this time using y-axis data
     plt.subplot(312)
-    plt.plot(timeArr,yArr, linewidth=0.25)
+    plt.plot(time_arr,y_arr, linewidth=0.25)
     plt.ylabel('By')	# side label
     plt.gca().axes.xaxis.set_ticklabels([]) # removing x axis numbers
     plt.autoscale(enable=True, axis='x', tight=True) # adjusting x axis scaling
@@ -270,14 +270,14 @@ def plot_Arrays(xArr, yArr, zArr, timeArr, filename, stime, etime, fileOption) :
     plt.gca().tick_params(left=True, right=True) # Putting ticks on both sides of y axis
     plt.gca().tick_params(axis='x', direction='in') # x axis ticks inverted
     plt.gca().tick_params(axis='y', direction='in') # y axis ticks inverted
-    if (defaultHoursFlag):
-        plt.xticks(defaultHoursArr) # setting the xaxis time ticks to 1 to 24 hours
+    if (default_hours_flag):
+        plt.xticks(default_hours_arr) # setting the xaxis time ticks to 1 to 24 hours
     else:
-        plt.xticks(hoursArr) # setting the xaxis time ticks to custom values
+        plt.xticks(hours_arr) # setting the xaxis time ticks to custom values
     
     ### Third plot using z-axis data. Add the x-axis label at the bottom
     plt.subplot(313)
-    plt.plot(timeArr,zArr, linewidth=0.25)
+    plt.plot(time_arr,z_arr, linewidth=0.25)
     plt.ylabel('Bz')	# side label
     plt.xlabel('Time in Hours') # label underneath
     plt.autoscale(enable=True, axis='x', tight=True) # adjusting x axis scaling
@@ -285,23 +285,23 @@ def plot_Arrays(xArr, yArr, zArr, timeArr, filename, stime, etime, fileOption) :
     plt.gca().tick_params(left=True, right=True) # Putting ticks on both sides of y axis
     plt.gca().tick_params(axis='x', direction='in') # x axis ticks inverted
     plt.gca().tick_params(axis='y', direction='in') # y axis ticks inverted
-    if (defaultHoursFlag):
-        plt.xticks(defaultHoursArr) # setting the xaxis time ticks to 1 to 24 hours
+    if (default_hours_flag):
+        plt.xticks(default_hours_arr) # setting the xaxis time ticks to 1 to 24 hours
     else:
-        plt.xticks(hoursArr) # setting the xaxis time ticks to custom values
+        plt.xticks(hours_arr) # setting the xaxis time ticks to custom values
 
-    fileOption = fileOption.lower()
-    if(fileOption == 'pdf'):
+    file_option = file_option.lower()
+    if(file_option == 'pdf'):
         # saving plot into a pdf file
         fig.savefig(filename + '.pdf', format='pdf', dpi=1200)
-    elif (fileOption == 'png'):
+    elif (file_option == 'png'):
         # saving plot into a png file
         fig.savefig(filename + '.png', format='png', dpi=1200)
-    elif (fileOption == 'no'):
+    elif (file_option == 'no'):
         # not saving plot but showing it instead
         plt.show()
     else :
-        print(fileOption + ' is not a supported filetype Option')
+        print(file_option + ' is not a supported filetype Option')
         sys.exit(0)# Exiting without an error code
 
 if __name__ == "__main__":
@@ -314,7 +314,7 @@ if __name__ == "__main__":
     filename = sys.argv[1]
 
     # file option
-    fileOption = 'pdf' # defaulting with the pdf option
+    file_option = 'pdf' # defaulting with the pdf option
 
     # try and catch block for bad file name
     try:
@@ -329,25 +329,25 @@ if __name__ == "__main__":
 
     ### If we get more than 2 items in the console/command line
     if len(sys.argv) == 3 : # if we have 3 items in the command line we assume that it is for specifying file type option
-        fileOption = sys.argv[2]
+        file_option = sys.argv[2]
     elif len(sys.argv) >= 4 :
         # iso format for a time is HH:MM:SS
         start_time = datetime.time.fromisoformat(sys.argv[2])
         end_time = datetime.time.fromisoformat(sys.argv[3])
     if len(sys.argv) == 5:
-        fileOption = sys.argv[4]
+        file_option = sys.argv[4]
     if len(sys.argv) >= 6:
         print( "TOO many items entered please try again!" ) # Not sure what else we should do but we should have something to handle if we get toooo many inputs.
         sys.exit(0) # Exiting without an error code
 
 
     ### Creating x, y, and z arrays -- NOW INCLUDING START AND END TIMES!!!
-    arrayX, arrayY, arrayZ, timeArr = create_Arrays(two_hz_binary_file, start_time, end_time)
+    arrayX, arrayY, arrayZ, time_arr = create_arrays(two_hz_binary_file, start_time, end_time)
 
     ### Plotting said arrays -- NOW INCLUDING START AND END TIMES AND FILE OPTION!!!
     # try and catch block to handle error in case file is already open
     try:
-        plot_Arrays(arrayX, arrayY, arrayZ, timeArr, filename, start_time, end_time, fileOption)
+        plot_arrays(arrayX, arrayY, arrayZ, time_arr, filename, start_time, end_time, file_option)
     except:
         print('Could not plot arrays to testgraph.pdf, file is open')
         sys.exit(0) # Exiting without an error code
