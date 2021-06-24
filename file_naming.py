3bf138781275432d34663d802ec7a11e76c5d506
Erik S. Steinmetz <steinmee@augsburg.edu>
2021-06-24 15:20:18 -0500

    Two create_time_interval_string functions, one with hour-minute-second input.

--- file_naming.py
+++ file_naming.py
@@ -7,15 +7,27 @@
 
 Functions that help create correct names for MACCS files.
 
+For example, a plot of a single station should have a name with the pattern
+twoLetterStationCodeLowerCase + yearday + timeInterval + "_l" + level + cadence + ".png" or ".pdf"
+where timeInterval is blank if it is the whole day and cadence is
+"_eighth_sec" or "_half_sec" or "_one_sec" or "_five_sec"
+
 """
 
-def create_time_interval_string( shour, sminute, ssecond, ehour, eminute, esecond) :
+def create_time_interval_string_hms( shour, sminute, ssecond, ehour, eminute, esecond) :
+    """
+        Creates a time interval string for a filename given the hour, minute, and second
+        of the start and end times.
+    """
     answer = ""
     if( shour != 0 or sminute != 0 or ssecond != 0 or ehour != 23 or eminute != 59 or esecond != 59) :
         answer = f"_{shour:02}{sminute:02}{ssecond:02}_{ehour:02}{eminute:02}{esecond:02}"
     return answer
     
 def create_time_interval_string( ssecond, esecond) :
+    """
+        Creates a time interval string for a filename given the start and end seconds of the day.
+    """
     shour = ssecond // 3600
     ssecond = ssecond % 3600
     sminute = ssecond // 60
@@ -24,8 +36,6 @@ def create_time_interval_string( ssecond, esecond) :
     esecond = esecond % 3600
     eminute = esecond // 60
     esecond = esecond % 60
-    return create_time_interval_string( shour, sminute, ssecond, ehour, eminute, esecond)
+    return create_time_interval_string_hms( shour, sminute, ssecond, ehour, eminute, esecond)
 
-if __name__ == "__main__" :
-    print( create_time_interval_string( 500, 84000))
 
