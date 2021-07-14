0dda242a423415199a80f961b8d02e4bfd2c6905
Ted Strombeck <ted.strombeck1@gmail.com>
2021-07-14 09:11:06 -0500

    Implemented read_raw_to_lists.create_lists_from_raw function in GUI

--- plotter_gui_strombeck.py
+++ plotter_gui_strombeck.py
@@ -560,7 +560,8 @@ def execute_functions(*args): # change to some other function name like "execute
         sys.exit(0)
     
     # Creating the arrays from the file
-    xArr, yArr, zArr, timeArr = raw_to_plot.create_arrays(file, start_time_stamp, end_time_stamp)
+    xArr, yArr, zArr, timeArr = read_raw_to_lists.create_lists_from_raw(file, start_time_stamp, end_time_stamp)
+    #xArr, yArr, zArr, timeArr = raw_to_plot.create_arrays(file, start_time_stamp, end_time_stamp)
 
     # Plotting the arrays
     raw_to_plot.plot_arrays(xArr, yArr, zArr, timeArr, file_name, start_time_stamp, end_time_stamp, file_save_as_option_value)
