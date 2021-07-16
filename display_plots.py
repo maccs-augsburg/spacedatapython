8108ed36c58e82e94a8d509da5650f3f2aeb4870
Ted Strombeck <ted.strombeck1@gmail.com>
2021-07-16 12:30:40 -0500

    Created display_plots.py file to be the main module for our displaying plots GUI

---
+++ display_plots.py
@@ -0,0 +1,15 @@
+# display_plots.py file
+#
+# June 2021 -- Created -- Ted Strombeck
+#
+
+from tkinter import *
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg)
+def main():
+    
+
+if __name__ == '__main__':
+    main()
+
