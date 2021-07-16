5d0c26c71f1eb8741ed4a5570c0c81848ad316a9
Ted Strombeck <ted.strombeck1@gmail.com>
2021-07-16 12:42:15 -0500

    Added z min and max functionality in toolbar_gui_entries function

--- canvas_test.py
+++ canvas_test.py
@@ -28,10 +28,12 @@ def create_toolbar():
 
 def toolbar_gui_entries(toolbar):
         ttk.Label(toolbar, text="Toolbar Entry Form").grid(column=1, row=1, sticky=(W,E))
-        ttk.Label(toolbar, text="Plot min x").grid(column=1, row=2)
-        ttk.Label(toolbar, text="Plot max x").grid(column=1, row=3)
-        ttk.Label(toolbar, text="Plot min y").grid(column=1, row=4)
-        ttk.Label(toolbar, text="Plot max y").grid(column=1, row=5)
+        ttk.Label(toolbar, text="Plot min x:").grid(column=1, row=2)
+        ttk.Label(toolbar, text="Plot max x:").grid(column=1, row=3)
+        ttk.Label(toolbar, text="Plot min y:").grid(column=1, row=4)
+        ttk.Label(toolbar, text="Plot max y:").grid(column=1, row=5)
+        ttk.Label(toolbar, text="Plot min z:").grid(column=1, row=6)
+        ttk.Label(toolbar, text="Plot max z:").grid(column=1, row=6)
 
 # plot function is created for plotting the graph in tkinter window
 def plot():
