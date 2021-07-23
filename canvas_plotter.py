a5374bc7a57f5e839787bb4e03cc0f630d7afd57
Ted Strombeck <ted.strombeck1@gmail.com>
2021-07-23 11:38:25 -0500

    Added imports to canvas_plotter.py file

--- canvas_plotter.py
+++ canvas_plotter.py
@@ -3,3 +3,9 @@
 # July 2021 -- Created -- Ted Strombeck
 #
 
+from tkinter import *
+from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkaggg import (FigureCanvasTkAgg)
+
