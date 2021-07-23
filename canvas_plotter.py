81aa2c3828c26dfef3f1e7906668f7b116631792
Ted Strombeck <ted.strombeck1@gmail.com>
2021-07-23 11:43:22 -0500

    Added create_initial_gui function with code

--- canvas_plotter.py
+++ canvas_plotter.py
@@ -9,8 +9,32 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkaggg import (FigureCanvasTkAgg)
 
+def create_initial_gui(window):
+    plot_button = Button(master = window,
+                         command = plot,
+                         height = 2,
+                         width = 10,
+                         text='Plot')
+    plot_button.grid(column=1, row=1)
+    
+    cancel_button = Button(master = window,
+                           command = cancel,
+                           height = 2,
+                           width = 10,
+                           text='Cancel')
+    cancel_button.grid(column=2, row=1)
+
 def main():
-    pass
+    window = Tk()
+
+    window.title('Plotting MACCS files')
+
+    window.geometry('200x100')
+
+    create_initial_gui(window)
+
+    window.mainloop()
+    
 
 if __name__ == "__main__":
     main()
