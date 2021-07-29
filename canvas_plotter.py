b4809782eb726398702833d121eb4c82f7f3d21a
Ted Strombeck <ted.strombeck1@gmail.com>
2021-07-29 14:02:16 -0500

    Adjusted start and end time stamps

--- canvas_plotter.py
+++ canvas_plotter.py
@@ -32,8 +32,8 @@ def plot(window, file_name_full):
 
         file = open(file_name_full, 'rb')
 
-        start_time_stamp = datetime.time(hour=11, minute=0, second=0)
-        end_time_stamp = datetime.time(hour=23, minute=59, second=59)
+        start_time_stamp = datetime.time(hour=18, minute=0, second=0)
+        end_time_stamp = datetime.time(hour=19, minute=59, second=59)
 
         xArr, yArr, zArr, timeArr = read_raw_to_lists.create_datetime_lists_from_raw(file,
                                                                         start_time_stamp,
