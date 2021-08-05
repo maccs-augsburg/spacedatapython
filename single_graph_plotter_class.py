9f6991fb542de0c102f66a684cdde8306f5eb2f9
Ted Strombeck <ted.strombeck1@gmail.com>
2021-08-05 10:02:26 -0500

    Initialized the single graph plotter class file

---
+++ single_graph_plotter_class.py
@@ -0,0 +1,13 @@
+# single_graph_plotter_class
+#
+# August 2021 -- Created -- Ted Strombeck
+#
+
+class SingleGraphPlotter:
+    pass
+
+def main():
+    pass
+
+if __name__ = "__main__":
+    main()
