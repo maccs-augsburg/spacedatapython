2a50a217773a75dad4ed15e149e1667ff31f5d82
Ted Strombeck <ted.strombeck1@gmail.com>
2021-08-05 10:11:19 -0500

    Finish __init__ function in file

--- single_graph_plotter_class.py
+++ single_graph_plotter_class.py
@@ -20,7 +20,40 @@ import read_raw_to_lists
 import canvas_plotter
 
 class SingleGraphPlotter:
-    pass
+    """ A class containing a GUI for a single graph, x, y, and z plotter.
+
+        Instance properties:
+        
+
+        Instance methods:
+            self.calculate( *args): ----------
+
+    """
+
+    def __init__(self):
+        root = Tk()
+        root.geometry('1400x800')
+        root.title("Plot input")
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        gui_labels(self, mainframe)
+
+        gui_entries(self, mainframe, root)
+        
+        for child in mainframe.winfo_children(): 
+            child.grid_configure(padx=5, pady=5)
+
+        station_code_entry.focus()
+
+        root.bind("<Return>", self.execute_functions)
+
+        root.mainloop()
+
+    def calculate(self, *args):
+        pass
 
 def main():
     pass
