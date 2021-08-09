ea92325adbb27a859bb9df40b70ba199661b8ba5
Annabelle <arnsa@augsburg.edu>
2021-08-09 13:51:45 -0500

    Testing file to see if it pushes

---
+++ class_try.py
@@ -0,0 +1,19 @@
+#Gui implemented with classes
+#Annabelle
+
+#Imports from tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+
+#imports from python 
+import sys
+import datetime
+from PIL import ImageTk, Image
+
+#imports from plotter functions
+import raw_to_plot
+import file_naming
+import read_raw_to_lists
+import one_array_plotted 
