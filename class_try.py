f277d4b6d6179cec4c989199d738d80bcd7a7137
Annabelle <arnsa@augsburg.edu>
2021-08-12 12:58:32 -0500

    Added the window in the class format

--- class_try.py
+++ class_try.py
@@ -17,3 +17,16 @@ import raw_to_plot
 import file_naming
 import read_raw_to_lists
 import one_array_plotted 
+
+
+class ThreeGraphPlotter:
+    """
+    """
+def __init__(self):
+    """
+    """
+    #Creation of the window 
+    window = Tk()
+    window.geometry('1500x600')
+    window.title('X, Y and Z Plotter')
+    
