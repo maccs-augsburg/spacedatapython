98332e7c42cd0d1f891e4ec3abfbec817351156f
Mark Ortega <mortegaponce27@gmail.com>
2022-05-23 11:53:28 -0500

    Making entry layout for line_x gui

--- linex_gui.py
+++ linex_gui.py
@@ -8,92 +8,135 @@ from PySide6.QtWidgets import (
 )
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QIcon
+# Adding this import for layout debugging
+from PySide6.QtGui import QPalette, QColor
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         # Why does tutorial sometimes pass MainWindow, and other times it doesnt
         super(MainWindow, self).__init__()
+
         self.setWindowTitle("MACCS Stacked Plots")
         self.setWindowIcon(QIcon("maccslogo_870.jpeg"))
+        self.setMinimumHeight(600)
+        self.setMinimumWidth(1100)
+        self.station_code = ""
         
         main_layout = QHBoxLayout()
-        entry_layout = QVBoxLayout()
+        #entry_layout = QVBoxLayout()
+        entry_layout = QFormLayout()
         '''
         Add widgets to entry_layout
         '''
-        station_line_edit = EntryBox("Station Code")
-        # station_line_edit.setMaxLength(5)
-        # station_line_edit.textEdited.connect(self.text_edited)
-        # station_code = None
-        # station_code_line_edit = EntryBox("Station Code: ")
-        # entry_layout.addWidget(station_code_line_edit)
-        entry_layout.addWidget(station_line_edit)
+        station_label = Label("Station Code: ")
+        station_edit = LineEdit()
+        year_day_label = Label("Year Day: ")
+        year_day_edit = LineEdit()
+        start_hour_label = Label("Start Hour: ")
+        start_hour_edit = LineEdit()
+        start_minute_label = Label("Start Minute: ")
+        start_minute_edit = LineEdit()
+        start_second_label = Label("Start Second: ")
+        start_second_edit = LineEdit()
+        end_hour_label = Label("End Hour: ")
+        end_hour_edit = LineEdit()
+        end_minute_label = Label("End Minute: ")
+        end_minute_edit = LineEdit()
+        end_second_label = Label("End Second: ")
+        end_second_edit = LineEdit()
+        min_x_label = Label("Plot Min X: ")
+        min_x_edit = LineEdit()
+        max_x_label = Label("Plot Max X: ")
+        max_x_edit = LineEdit()
+        min_y_label = Label("Plot Min Y: ")
+        min_y_edit = LineEdit()
+        max_y_label = Label("Plot Max Y: ")
+        max_y_edit = LineEdit()
+        min_z_label = Label("Plot Min Z: ")
+        min_z_edit = LineEdit()
+        max_z_label = Label("Plot Max Z: ")
+        max_z_edit = LineEdit()
+        entry_layout.addRow(station_label, station_edit)
+        entry_layout.addRow(year_day_label, year_day_edit)
+        entry_layout.addRow(start_hour_label, start_hour_edit)
+        entry_layout.addRow(start_minute_label, start_minute_edit)
+        entry_layout.addRow(start_second_label, start_second_edit)
+        entry_layout.addRow(end_hour_label, end_hour_edit)
+        entry_layout.addRow(end_minute_label, end_minute_edit)
+        entry_layout.addRow(end_second_label, end_second_edit)
+        entry_layout.addRow(min_x_label, min_x_edit)
+        entry_layout.addRow(max_x_label, max_x_edit)
+        entry_layout.addRow(min_y_label, min_y_edit)
+        entry_layout.addRow(max_y_label, max_y_edit)
+        entry_layout.addRow(min_z_label, min_z_edit)
+        entry_layout.addRow(max_z_label, max_z_edit)
+
+
         main_layout.addLayout(entry_layout)
+        main_layout.addWidget(Color('green'))
 
         widget = QWidget()
         widget.setLayout(main_layout)
-        self.centralWidget(widget)
+        self.setCentralWidget(widget)
         
-        # entry_layout.addWidget()
-        # # QLine Edit, maybe I will use
-        # def return_pressed(self):
-        #     pass
-        # def text_changed(self, s):
-        #     # Assign s to station code, current entry in line_edit
-        #     # Returns string value
-        #     self.station_code = s
+# class attributes are before init, they are like static variables?
+# instance attributes are after init, they are unique to each one, more of what I want
 
 class LineEdit(QLineEdit):
+    ###########################################
+    #Anything in here is a static variable#####
+    ###########################################
+    # this explains the super() keyword and various variations you might see used in tutorials
+    #https://stackoverflow.com/questions/1607612/python-how-do-i-make-a-subclass-from-a-superclass
     def __init__(self):
-        super(LineEdit, self).__init__()
-
+        super().__init__()
+        #########################################################################
+        #Anything in here is an instance variable as long as it has self keyword#
+        #########################################################################
         self.setMaxLength(7)
         self.textEdited.connect(self.text_edited)
-        self.entry
+        self.entry = "N/A"
+    #######################################################################
+    #Methods containing self, are instance methods#########################
+    #######################################################################
+    def text_edited(self, s):
+        # s is a string, confirmed wity print(type(s))
+        self.entry = s
+        # Don't have to pass self again, implied, but weird
+        print(self.get_entry())
 
-        def text_edited(self, s):
-            pass
+    def get_entry(self):
+        return self.entry
 
-class EntryBox(QLineEdit, QLabel):
-
-    entry = None
-    # similar to constructor
+class Label(QLabel):
     def __init__(self, label_name):
-        super(EntryBox, self).__init__()
+        super().__init__()
 
         self.setText(label_name)
-        self.textEdited.connect(self.text_edited)
-        # If it inherites from both, and we use QLabel.setBuddy to link to QLineEdit
-        # Will this line even work? Purpose is to get qlable and qlineedit on the same line
-        # vs. stacked
-        self.setBuddy(self)
+        font = self.font()
+        font.setPointSize(12)
+        self.setFont(font)
 
-        def text_edited(self,s):
-            entry = s
+class Color(QWidget):
 
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
 
-# class EntryBox(QLineEdit):
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
+    
+# class EntryBox(LineEdit, Label):
 
 #     def __init__(self, label_name):
-#         super(EntryBox, self).__init__()
+#         super().__init__()
 
-#         label = QLabel(label_name)
-#         # textEdited is a "signal", override corresponding method for custom us
-#         #self.line_edit.textEdited.connect(self.text_edited)
-#         self.textEdited.connect(self.text_edited)
-#         self.setMaxLength(10)
-#         entry = None
+#         self.line_edit = LineEdit()
+#         self.label = Label(label_name)
 
-#         label.setBuddy(self)
-
-#         def text_edited(self, s):
-#             print("textEdited")
-#             self.entry = s
-        
-#         def get_entry(self):
-#             return self.entry
-
+#         self.label.setBuddy(self.line_edit)
 
 def main ():
 
