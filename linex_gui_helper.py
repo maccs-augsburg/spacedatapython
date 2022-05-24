2e6fc0b9adbac06380a542e4782f9fb2b7744a52
Mark Ortega <mortegaponce27@gmail.com>
2022-05-24 12:16:20 -0500

    Deleted extra imports, comments

--- linex_gui_helper.py
+++ linex_gui_helper.py
@@ -1,34 +1,23 @@
 from PySide6.QtWidgets import (
-    QMainWindow, QApplication, QLabel,
-    QToolBar, QStatusBar, QCheckBox,
-    QHBoxLayout, QVBoxLayout, QGridLayout,
-    QLineEdit, QWidget, QFormLayout, QFileDialog
+    QMainWindow, QLabel, QLineEdit
 )
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QPalette, QColor, QIntValidator
+from PySide6.QtGui import QPalette, QColor
 
-# class attributes are before init, they are like static variables?
-# instance attributes are after init, they are unique to each one, more of what I want
 class LineEdit(QLineEdit):
-    ###########################################
-    #Anything in here is a static variable#####
-    ###########################################
-    # this explains the super() keyword and various variations you might see used in tutorials
-    #https://stackoverflow.com/questions/1607612/python-how-do-i-make-a-subclass-from-a-superclass
+
     def __init__(self):
         super().__init__()
         #########################################################################
         #Anything in here is an instance variable as long as it has self keyword#
         #########################################################################
         self.setMaxLength(7)
-        # commented out, don't see a way to unset for station code
-        #self.setValidator(QIntValidator())
-        # only accept digits
         # https://doc.qt.io/qt-5/qlineedit.html
         self.setInputMask("9999999")
         self.textEdited.connect(self.text_edited)
         self.entry = "N/A"
         self.setMaximumWidth(80)
+
     #######################################################################
     #Methods containing self, are instance methods#########################
     #######################################################################
@@ -39,18 +28,8 @@ class LineEdit(QLineEdit):
         print(self.get_entry())
 
     def get_entry(self):
-        # TODO: Assert entries with QLineEdit methods, have to workaround Station Names 3 letters followed by numbers
-        '''
-        if XXX = letters then return as string
-
-        if XXXXXX = all nums return as int
-        '''
         return self.text()
-    '''
-    When we open a new file, want to reset the entries to some default value
-    start min/sec/hour to (0, 0, 0)
-    end min/sec/hour (23, 59, 59)
-    '''
+
     def set_entry(self, new_entry):
         
         self.entry = new_entry
@@ -77,12 +56,3 @@ class Color(QWidget):
         palette.setColor(QPalette.Window, QColor(color))
         self.setPalette(palette)
     
-# class EntryBox(LineEdit, Label):
-
-#     def __init__(self, label_name):
-#         super().__init__()
-
-#         self.line_edit = LineEdit()
-#         self.label = Label(label_name)
-
-#         self.label.setBuddy(self.line_edit)
