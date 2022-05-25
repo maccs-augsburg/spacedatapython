a150b276f46af64cfbccebf6eb3d2aa6ad70ab21
Christopher J Hance <hance@augsburg.edu>
2022-05-25 16:26:42 -0500

    changing error and warning pop ups to the respective nature and waht they say as well

--- entry_checks.py
+++ entry_checks.py
@@ -6,6 +6,7 @@
 from tkinter import messagebox
 
 import one_array_plotted
+import line_XYZ
 import sys
 from PyQt6.QtWidgets import QMessageBox
 def year_day_entry_check( self,year_day_value):
@@ -18,7 +19,7 @@ def year_day_entry_check( self,year_day_value):
     """
     # Checking to see if any input was put in the yearday entry box
     if (len(year_day_value) == 0):
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
 def station_names_entry_check(self,station_names_value):
 
     """
@@ -30,7 +31,7 @@ def station_names_entry_check(self,station_names_value):
     # Checking to see if no input was put in the station code entry box
     if(len(station_names_value) == 0):
         # show error as no input was received
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
 
 def file_format_entry_check(self,selection_file_value):
     """
@@ -46,17 +47,17 @@ def file_format_entry_check(self,selection_file_value):
     """
     if(selection_file_value == '1'):
         # CDA-Web branch (NOT IMPLEMENTED)
-        warning_message_pop_up(title="File format option error", message="Sorry! But we don't have this option available yet, please try picking a different option")
+        line_XYZ.MainWindow.warning_message_pop_up(self, "File format option error", "Sorry! But we don't have this option available yet, please try picking a different option")
 
     # Testing to see if user selected IAGA2000 branch
     elif(selection_file_value == '2'):
         #IAGA2000 branch (NOT IMPLEMENTED)
-        warning_message_pop_up(title="File format option error", message="Sorry! But we don't have this option available yet, please try picking a different option")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"File format option error", "Sorry! But we don't have this option available yet, please try picking a different option")
 
     # Testing to see if user selected IAGA2002 branch
     elif(selection_file_value == '3'):
         #IAGA2002 branch (NOT IMPLEMENTED)
-        warning_message_pop_up(title="File format option error", message="Sorry! But we don't have this option available yet, please try picking a different option")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"File format option error", "Sorry! But we don't have this option available yet, please try picking a different option")
 
     # Testing to see if user selected Raw 2hz branch
     elif(selection_file_value == '4'):
@@ -70,12 +71,12 @@ def file_format_entry_check(self,selection_file_value):
     # Otherwise we can assume that no option had been selected
     else:
         # Message box error when no file format option has been selected
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
 
     # Returning the string of the file type to be used
     return file_ending_value
 
-def graph_from_plotter_entry_check(graph_from_plotter_value_x,graph_from_plotter_value_y, graph_from_plotter_value_z, xArr, yArr, zArr, timeArr, filename, stime, etime, selection_file):
+def graph_from_plotter_entry_check(self,graph_from_plotter_value_x,graph_from_plotter_value_y, graph_from_plotter_value_z, xArr, yArr, zArr, timeArr, filename, stime, etime, selection_file):
 
     """
     Checks the gui entries for plotting, x, y, z axis. If values are set, then we plot those axis
@@ -155,7 +156,7 @@ def graph_from_plotter_entry_check(graph_from_plotter_value_x,graph_from_plotter
             
     #Warning Message
     else:
-        warning_message_pop_up(title = "File Format Option Error", message = "Please select a file format option")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"File Format Option Error",  "Please select a file format option")
 
     #return graph_from_plotter_value_x, graph_from_plotter_value_y, graph_from_plotter_value_z, fig
     return fig
@@ -178,12 +179,12 @@ def start_hour_entry_check(self,start_hour_string):
 
     if(value > 23):
         # Have error message box pop up because it can't be more than 23
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
         
     # Testing to see if the inputted value is less than what it can be
     elif(value < 0):
         # Have error message box pop up because it can't be a negative number
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
 
     # Returning the start_hour_string so that whatever changes we made to it get returned
     return value
@@ -206,12 +207,12 @@ def start_minute_entry_check(self,start_minute_string):
 
     if(value > 59):
         # Have error messsage box pop up becuase it can't be more than 59
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
 
     # Testing to see if the inputted value is less than what it can be
     elif(value < 0):
         # Have error message box pop up because it can't be a negative number
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
 
     # Returning the start_minute_string so whatever changes we made to it get returned
     return value
@@ -234,12 +235,12 @@ def start_second_entry_check(self,start_second_string):
 
     if(value > 59):
         # Have error messsage box pop up becuase it can't be more than 59
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in Start Second box \nValue too large\nPlease make sure value is less than 59")
 
     # Testing to see if the inputted value is less than what it can be
     elif(value < 0):
         # Have error message box pop up because it can't be a negative number
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in Start Second box \nValue too small\nPlease make sure value is greater or equal to 0")
 
     # Returning the start_second_string so whatever changes we made to it get returned
     return value
@@ -263,12 +264,12 @@ def end_hour_entry_check(self,end_hour_string):
     # Testing to see if the inputted value exceeds what it can be
     if(value > 23):
         # Have error message box pop up because it can't be more than 23
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in End Hour box \nValue too large\nPlease make sure value is less than 24")
 
         # Testing to see if the inputted value is less than what it can be
     elif(value < 0):
         # Have error message box pop up because it can't be a negative number
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in End Hour box \nValue too small\nPlease make sure value is greater or equal to 0")
 
     # Returning the end_hour_string so whatever changes we made to it get returned
     return value
@@ -292,12 +293,11 @@ def end_minute_entry_check(self,end_minute_string):
     # Testing to see if the inputted value exceeds what it can be
     if(value > 59):
         #Have error messsage box pop up becuase it can't be more than 59
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
-
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in End Min box \nValue too large\nPlease make sure value is less than 59")
     # Testing to see if the inputted value is less than what it can be
     elif(value < 0):
         # Have error message box pop up because it can't be a negative number
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in End Min box \nValue too small\nPlease make sure value is greater or equal to 0")
 
     # Returning the end_minute_string so whatever changes we made to it get returned
     return value
@@ -320,21 +320,14 @@ def end_second_entry_check(self,end_second_string):
     # Testing to see if the inputted value exceeds what it can be
     if(value > 59):
         # Have error messsage box pop up becuase it can't be more than 59
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in End Sec box \nValue too large\nPlease make sure value is less than 59")
 
     # Testing to see if the inputted value is less than what it can be
     elif(value < 0):
         # Have error message box pop up because it can't be a negative number
-        error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Invalid time entry in End Sec box \nValue too small\nPlease make sure value is greater or equal to 0")
 
     # Returning the end_second_string so whatever changes we made to it get returned
     return value
 
-def error_message_pop_up(self,title, message):
-    error_mes = QMessageBox.critical(self, title, message)
-    sys.exit(0)
-
-def warning_message_pop_up(title, message):
-    # pops up warning message box with the title and message inputted
-    messagebox.showwarning(title=title, message="WARNING: " + message)
         
