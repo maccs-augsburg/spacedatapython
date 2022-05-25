942541f449ef9cec31906686ceba6c961d09f008
Christopher J Hance <hance@augsburg.edu>
2022-05-25 16:30:43 -0500

    complete proper error and warning pop ups formating

--- entry_checks.py
+++ entry_checks.py
@@ -19,7 +19,7 @@ def year_day_entry_check( self,year_day_value):
     """
     # Checking to see if any input was put in the yearday entry box
     if (len(year_day_value) == 0):
-        line_XYZ.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in Year Day box \nValue invalid; Most likely empty\nPlease make sure value on file is correct")
 def station_names_entry_check(self,station_names_value):
 
     """
@@ -179,12 +179,12 @@ def start_hour_entry_check(self,start_hour_string):
 
     if(value > 23):
         # Have error message box pop up because it can't be more than 23
-        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in Start Min box \nValue too large\nPlease make sure value is less than 24")
         
     # Testing to see if the inputted value is less than what it can be
     elif(value < 0):
         # Have error message box pop up because it can't be a negative number
-        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in Start Min box \nValue too large\nPlease make sure value is greater or equal to 0")
 
     # Returning the start_hour_string so that whatever changes we made to it get returned
     return value
@@ -207,12 +207,12 @@ def start_minute_entry_check(self,start_minute_string):
 
     if(value > 59):
         # Have error messsage box pop up becuase it can't be more than 59
-        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in Start Min box \nValue too large\nPlease make sure value is less than 60")
 
     # Testing to see if the inputted value is less than what it can be
     elif(value < 0):
         # Have error message box pop up because it can't be a negative number
-        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Couldn't find and open your file \nPlease make sure you select proper file \nExiting program")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in Start Min box \nValue too large\nPlease make sure value is greater or equal to 0")
 
     # Returning the start_minute_string so whatever changes we made to it get returned
     return value
@@ -235,7 +235,7 @@ def start_second_entry_check(self,start_second_string):
 
     if(value > 59):
         # Have error messsage box pop up becuase it can't be more than 59
-        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in Start Second box \nValue too large\nPlease make sure value is less than 59")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in Start Second box \nValue too large\nPlease make sure value is less than 60")
 
     # Testing to see if the inputted value is less than what it can be
     elif(value < 0):
@@ -293,7 +293,7 @@ def end_minute_entry_check(self,end_minute_string):
     # Testing to see if the inputted value exceeds what it can be
     if(value > 59):
         #Have error messsage box pop up becuase it can't be more than 59
-        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in End Min box \nValue too large\nPlease make sure value is less than 59")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in End Min box \nValue too large\nPlease make sure value is less than 60")
     # Testing to see if the inputted value is less than what it can be
     elif(value < 0):
         # Have error message box pop up because it can't be a negative number
@@ -320,12 +320,12 @@ def end_second_entry_check(self,end_second_string):
     # Testing to see if the inputted value exceeds what it can be
     if(value > 59):
         # Have error messsage box pop up becuase it can't be more than 59
-        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in End Sec box \nValue too large\nPlease make sure value is less than 59")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"Time Entry Error", "Invalid time entry in End Sec box \nValue too large\nPlease make sure value is less than 60")
 
     # Testing to see if the inputted value is less than what it can be
     elif(value < 0):
         # Have error message box pop up because it can't be a negative number
-        line_XYZ.MainWindow.error_message_pop_up(self,"File open error", "Invalid time entry in End Sec box \nValue too small\nPlease make sure value is greater or equal to 0")
+        line_XYZ.MainWindow.warning_message_pop_up(self,"File open error", "Invalid time entry in End Sec box \nValue too small\nPlease make sure value is greater or equal to 0")
 
     # Returning the end_second_string so whatever changes we made to it get returned
     return value
