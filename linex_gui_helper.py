c6427dbafec3f0a9cf2e34d91c3db9130c8e3285
Mark Ortega <mortegaponce27@gmail.com>
2022-05-25 09:35:38 -0500

    Added note about weird side effect coming from .setInputMask on LineEdit class

--- linex_gui_helper.py
+++ linex_gui_helper.py
@@ -1,9 +1,10 @@
 from PySide6.QtWidgets import (
-    QMainWindow, QLabel, 
+    QMainWindow, QLabel,
     QLineEdit, QWidget
 )
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPalette, QColor
+
 
 class LineEdit(QLineEdit):
 
@@ -12,10 +13,16 @@ class LineEdit(QLineEdit):
         #########################################################################
         #Anything in here is an instance variable as long as it has self keyword#
         #########################################################################
-        self.setMaxLength(7)
+        self.setMaxLength(5)
         # https://doc.qt.io/qt-5/qlineedit.html
-        self.setInputMask("9999999")
+
+        # Weird side effect,
+        # Turns cursor black, and lets you put cursor in any spot inside the QLineEdit entries
+        # It counts empty spaces as part of the limit, so could be annoying deleting blank spaces
+        self.setInputMask("99999")
+        # self.setFocus()
         self.textEdited.connect(self.text_edited)
+        self.setAlignment(Qt.AlignCenter)
         self.entry = "N/A"
         self.setMaximumWidth(80)
 
@@ -30,9 +37,10 @@ class LineEdit(QLineEdit):
         return self.text()
 
     def set_entry(self, new_entry):
-        
+
         self.entry = new_entry
         self.setText(str(new_entry))
+
 
 class Label(QLabel):
     def __init__(self, label_name):
@@ -45,6 +53,7 @@ class Label(QLabel):
         self.setAlignment(Qt.AlignLeft)
         self.setMaximumWidth(80)
 
+
 class Color(QWidget):
 
     def __init__(self, color):
@@ -54,4 +63,3 @@ class Color(QWidget):
         palette = self.palette()
         palette.setColor(QPalette.Window, QColor(color))
         self.setPalette(palette)
-    
