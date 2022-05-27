38c8dd8704f83dfe6dccb05a3b7bb5bf3612b2d3
Mark Ortega <mortegaponce27@gmail.com>
2022-05-27 13:12:12 -0500

    Deleted old class from matplotlib tutorial

--- StackedPlotsGui/linex_gui_helper.py
+++ StackedPlotsGui/linex_gui_helper.py
@@ -5,33 +5,22 @@ from PySide6.QtWidgets import (
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPalette, QColor
 
-import matplotlib
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-from matplotlib.figure import Figure
 
 class LineEdit(QLineEdit):
 
     def __init__(self):
         super().__init__()
-        #########################################################################
-        #Anything in here is an instance variable as long as it has self keyword#
-        #########################################################################
         self.setMaxLength(5)
         # https://doc.qt.io/qt-5/qlineedit.html
-
         # Weird side effect,
         # Turns cursor black, and lets you put cursor in any spot inside the QLineEdit entries
         # It counts empty spaces as part of the limit, so could be annoying deleting blank spaces
         self.setInputMask("99999")
-        # self.setFocus()
         self.textEdited.connect(self.text_edited)
         self.setAlignment(Qt.AlignCenter)
         self.entry = "N/A"
         self.setMaximumWidth(80)
 
-    #######################################################################
-    #Methods containing self, are instance methods#########################
-    #######################################################################
     def text_edited(self, s):
         self.entry = s
         print(self.get_entry())
@@ -60,14 +49,6 @@ class Label(QLabel):
         self.setMaximumWidth(80)
 
 
-class PlotCanvas(FigureCanvasQTAgg):
-
-    def __init__(self, parent = None, width = 5, height = 4, dpi = 100):
-        fig = Figure(figsize = (width, height), dpi = dpi)
-        self.axes = fig.add_subplot(111)
-        super(PlotCanvas, self).__init__(fig)
-
-
 class Color(QWidget):
 
     def __init__(self, color):
@@ -77,4 +58,3 @@ class Color(QWidget):
         palette = self.palette()
         palette.setColor(QPalette.Window, QColor(color))
         self.setPalette(palette)
-
