12e3f39a15cca272efb7f266c70e9005df59dd93
Mark Ortega <mortegaponce27@gmail.com>
2022-06-06 10:06:07 -0500

    Added more widgets

--- StackedPlotsGui/custom_widgets.py
+++ StackedPlotsGui/custom_widgets.py
@@ -6,11 +6,12 @@ May 2022 -- Created -- Mark Ortega-Ponce
 
 from PySide6.QtWidgets import (
     QMainWindow, QLabel,
-    QLineEdit, QWidget
+    QLineEdit, QWidget, QCheckBox, QPushButton
 )
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPalette, QColor
 
+FONT_SIZE = 12
 
 class LineEdit(QLineEdit):
     '''
@@ -28,7 +29,7 @@ class LineEdit(QLineEdit):
         self.setAlignment(Qt.AlignCenter)
         #self.setAlignment(Qt.AlignLeft)
         self.entry = "N/A"
-        self.setMaximumWidth(80)
+        self.setMaximumWidth(95)
 
     def text_edited(self, s):
         self.entry = s
@@ -52,10 +53,89 @@ class Label(QLabel):
 
         self.setText(label_name)
         font = self.font()
-        font.setPointSize(14)
+        font.setPointSize(FONT_SIZE)
         self.setFont(font)
         self.setAlignment(Qt.AlignLeft)
         self.setMaximumWidth(85)
+
+class CheckBox(QCheckBox):
+    def __init__(self, label_name):
+        super().__init__()
+
+        self.setText(label_name)
+        font = self.font()
+        font.setPointSize(FONT_SIZE)
+        self.setFont(font)
+        self.setMaximumWidth(30)
+
+
+class PushButton(QPushButton):
+    def __init__(self, label_name):
+        super().__init__()
+
+        self.setText(label_name)
+        font = self.font()
+        self.setFont(font)
+        font.setPointSize(FONT_SIZE)
+        self.setCheckable(True)
+        self.button_is_checked = False
+        self.clicked.connect(self.button_toggle)
+        self.setChecked(self.button_is_checked)
+        self.setMaximumWidth(145)
+
+    def button_toggle(self, checked):
+        self.button_is_checked = checked
+        # if checked:
+        #     self.setText("Stacked (X, Y, Z)")
+        # else:
+        #     pass
+        #     self.setText("Single Plot (X, Y, Z)")
+        print(self.button_is_checked)
+    
+    def is_toggled(self):
+        return self.button_is_checked
+
+
+# class ToggleButton (QCheckBox):
+#     def __init__(self, width = 60, bg_color = '#777', circle_color = "#DDD", active_color = "#00Bcff"):
+#         super().__init__()
+
+#         self.bg_color = bg_color
+#         self.circle_color = circle_color
+#         self.active_color = active_color
+
+#         self.stateChanged.connect(self.state_change)
+
+#     def state_change(self, pos: QPoint):
+#         print("Status ", self.isChecked())
+
+#     def hitButton(self, pos: QPoint):
+#         return self.contentsRect().contains(pos)
+
+#     def paintEvent(self, e):
+#         p = QPainter(self)
+#         p.setRenderHint(QPainter.AntiAliasing)
+#         # Set as no pen
+#         p.setPen(Qt.NoPen)
+
+#         rect = QRect(0,0,self.width(), self.height())
+
+#         if not self.is_checked():
+
+#             p.setBrush(QColor(self.bg_color))
+#             p.drawRoundedRect(0, 0, rect.width(), self.height(), self.height()/2, self.width() / 2)
+#             p.setBrush(QColor(self.circle_color))
+#             p.drawEllipse(3, 3, 22, 22)
+    
+#         else:
+#             p.setBrush(QColor(self.bg_color))
+#             p.drawRoundedRect(0, 0, rect.width(), self.height(), self.height()/2, self.width() / 2)
+#             p.setBrush(QColor(self.active_color))
+#             p.drawEllipse(3, 3, 22, 22)
+            
+#         # Close Q painter pen, else error
+#         p.end()
+
 
 class Color(QWidget):
 
