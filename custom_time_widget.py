f0dad33dba4d101f518ab99cd58676c69fe73bf1
Christopher J Hance <hance@augsburg.edu>
2022-06-09 11:38:03 -0500

    SORRY LONG COMIT create custom time widget that has a min and max time buttons in new custom_time_widget.py subject to change name and add comments rename etc

---
+++ custom_time_widget.py
@@ -0,0 +1,86 @@
+from PySide6.QtWidgets import (QMainWindow, QApplication, 
+                                QLabel, QLineEdit, 
+                                QWidget, QHBoxLayout, 
+                                QGridLayout,QPushButton, 
+                                QToolBar,QVBoxLayout,
+                                QFileDialog, QRadioButton,
+                                QCheckBox,QMessageBox, QButtonGroup, QTimeEdit, 
+                                )
+from PySide6.QtGui import QIcon, QAction, QPixmap, Qt
+from PySide6.QtCore import  QSize, QTime, QRect
+from PySide6 import QtWidgets
+from PySide6 import QtGui
+from PySide6 import QtCore
+# path for file open 
+from pathlib import Path
+
+#imports from python 
+import sys
+import datetime
+
+
+
+class MinMaxTime(QtWidgets.QWidget):
+    
+    def __init__(self, *args,**kwargs):
+
+        super(MinMaxTime,self).__init__(*args,**kwargs)
+
+        main_layout = QtWidgets.QHBoxLayout()
+        button_layout = QtWidgets.QVBoxLayout()
+
+        self.setSizePolicy(
+            QtWidgets.QSizePolicy.Maximum,
+            QtWidgets.QSizePolicy.Maximum
+        )
+
+        self.qtime_start_time = QTimeEdit()
+        self.qtime_start_time.setTimeRange(QTime(00,00,00), QTime(24,00,00))
+        self.qtime_start_time.setDisplayFormat('hh:mm:ss')
+        self.qtime_start_time.setFixedWidth(75)
+
+        self.label_one = QPushButton("Min?")
+        self.label_two = QPushButton("Max?")
+
+        self.label_two.clicked.connect(self.set_max_time)
+        self.label_one.clicked.connect(self.set_min_time)
+
+        main_layout.addWidget(self.qtime_start_time)
+        button_layout.addWidget(self.label_one)
+        button_layout.addWidget(self.label_two)
+        main_layout.addLayout(button_layout)
+
+
+        self.setLayout(main_layout)        
+
+
+    #set Qtime() to max 23 59 59 
+    def set_max_time(self):
+        self.qtime_start_time.setTime(QTime(23,59,59))
+
+    #sets QTime() to min 00 00 00
+    def set_min_time(self):
+        self.qtime_start_time.setTime(QTime(00,00,00))
+
+
+    # MAX SIZE OF WIDget
+    def sizeHint(self):
+        return QtCore.QSize(20,20)
+    
+    # background paint of widget
+    def paintEvent(self, e):
+        painter = QtGui.QPainter(self)
+        brush = QtGui.QBrush()
+        brush.setColor(QtGui.QColor('blue'))
+        brush.setStyle(Qt.SolidPattern)
+        rect = QRect(0,0, painter.device().width(), painter.device().height())
+        painter.fillRect(rect,brush)
+
+def main():
+    app = QApplication(sys.argv)
+    window = MinMaxTime()
+    window.show()
+    app.exec()
+
+if __name__ == "__main__":
+    main()
