3b72664be5318a89752caf0fd497b9d75a90b7e7
Christopher J Hance <hance@augsburg.edu>
2022-06-21 12:38:23 -0500

    removed unused imports

--- x_axis_time_formatter.py
+++ x_axis_time_formatter.py
@@ -2,17 +2,10 @@
 #
 # August 2021 -- Created -- Ted Strombeck
 #
-
 # Python 3 imports
-import sys
 import datetime
 
-# MACCS imports
-from raw_codecs import decode, time_of_record
-
 # Matplotlib imports
-import matplotlib.pyplot as plt
-from matplotlib.ticker import(MultipleLocator, AutoMinorLocator)
 import matplotlib.dates as mdates
 
 def create_time_list( stime, etime):
