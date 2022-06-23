3dd2c33d8a6ba5518c0919242d48b20fd56d276b
Christopher J Hance <hance@augsburg.edu>
2022-06-23 12:51:58 -0500

    New combined gui in works between chris and mark

---
+++ newGui/new_gui.py
@@ -0,0 +1,146 @@
+# Mark Ortega-Ponce & Chris Hance 
+
+#Import from PySide6 // QT
+from PySide6.QtWidgets import (QMainWindow, QApplication, 
+                                QLabel, QLineEdit, 
+                                QWidget, QHBoxLayout, 
+                                QGridLayout,QPushButton, 
+                                QToolBar,QVBoxLayout,
+                                QFileDialog, QRadioButton,
+                                QCheckBox,QMessageBox, 
+                                QButtonGroup, QSizePolicy
+                                )
+from PySide6.QtGui import QIcon, QAction, QPixmap, Qt, QPalette
+from PySide6.QtCore import  QSize, QTime
+
+# path for file open 
+from pathlib import Path
+
+#imports from python 
+import sys
+import datetime
+
+#Imports from matplotlib
+import matplotlib
+from matplotlib.ft2font import LOAD_IGNORE_GLOBAL_ADVANCE_WIDTH
+matplotlib.use('qtagg')
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg, NavigationToolbar2QT
+
+from custom_widgets import (
+    LineEdit, Label, CheckBox, 
+    PushButton, Spinbox, Time, 
+    Layout, HLayout,
+    Toolbar, VLayout)
+
+class MainWindow(QMainWindow):
+    """
+
+    Using PySide6 creates GUI that display and visualizes data coming from the Magtometers in the artic from files currently
+    in .2hz or .s2 format. 
+    This Class holds all GUI related functions and widgets to display the proper information. 
+
+    """
+    def __init__(self):
+
+        """ 	
+        All the widgets so many o.o
+        
+        :type self:
+        :param self:
+    
+        :raises:
+    
+        :rtype:
+        """    
+        super().__init__()
+        self.setWindowTitle("MACCS Plotting Program")
+
+        self.setGeometry(60,60, 1000,800)
+        
+    def __call__(self,event):
+        '''
+        __call__ is the event listener connected to matplotlib 
+        it will listen for mouse clicks and record the xdata of the first and second click and converting them from matplotlib dates to datetime objects
+        and then stripping the time down to hour min second spliting the values and settime our Time widget to that time and replotting normally so the scale is auto as it aslways is 
+
+        '''
+        datetime_object = mdates.num2date(event.xdata)
+        time_value_on_click= datetime.datetime.strftime(datetime_object,"%H %M %S")
+        zoom_values = time_value_on_click.split(" ")
+        
+        if self.temp_var == 0:
+            self.custom_start_time.time_widget.setTime(QTime(int(zoom_values[0]),int(zoom_values[1]),int(zoom_values[2])))
+
+        elif self.temp_var == 1:
+            self.custom_end_time.time_widget.setTime(QTime(int(zoom_values[0]),int(zoom_values[1]),int(zoom_values[2])))
+
+            self.graph.mpl_disconnect(self.cid)
+            if self.button_plot_stacked_graph.isHidden() == True:
+                self.plot_three_axis()
+            elif self.button_plot_three_axis.isHidden() == True:
+                self.plot_stacked_axis()
+        self.temp_var = self.temp_var + 1
+
+    def zoom_in_listener(self):
+        '''
+        Starts event listening that listens for clicks after clicking the zoom icon
+        and uses the __call__ function to handle to clicks 
+        and we get two clicks we call other function in __call__ after zoom_in_listner is called after button events happened
+        the values are reset so we can continune more zoomin 
+        '''
+        self.cid = self.graph.mpl_connect('button_press_event', self)
+        if self.temp_var > 1:
+            self.temp_var = 0
+
+    def save(self):
+        """
+            Saves the Graph as a PDF Image
+
+            file_type is a QMessageBox that pops up once the Save as QButton is pressed 
+        """
+
+        question_box = QMessageBox(self)
+
+        save_image_button = question_box.addButton(str('Save Image'), QMessageBox.ActionRole)
+        cancel_button = question_box.addButton(str('Cancel'),QMessageBox.ActionRole)
+        window_title_text = question_box.setWindowTitle("Save Image")
+        message_text = question_box.setText("Would you like to save this image as a PDF?")
+        start = question_box.exec()
+
+        if question_box.clickedButton() == save_image_button:
+            plt.savefig(self.file_name + ".pdf")
+            subprocess.Popen(self.file_name + '.pdf', shell=True)
+        elif question_box.clickedButton() == cancel_button:
+            question_box.close()
+
+    def save_as(self):
+         
+        """
+            Saves the Graph as an image with user chosing either a PDF or PNG option 
+            can easily incorperate more files as needed 
+
+            file_type is a QMessageBox that pops up once the Save as QButton is pressed 
+        """
+        #def information (parent, title, text, button0[, button1=QMessageBox.StandardButton.NoButton])
+
+        file_type = QMessageBox(self)
+
+        pdf_button = file_type.addButton(str("PDF"), QMessageBox.ActionRole)
+        png_button = file_type.addButton(str('PNG'), QMessageBox.ActionRole)
+        cancel_button = file_type.addButton(str('Cancel'), QMessageBox.ActionRole)
+
+        window_title_text = file_type.setWindowTitle("Save as...")
+        message_text = file_type.setText("Select would image type you would like to save as... \n PDF or PNG")
+
+        start = file_type.exec()
+
+        if file_type.clickedButton() == pdf_button:
+            plt.savefig(self.file_name + ".pdf")
+            subprocess.Popen(self.file_name + '.pdf', shell=True)
+        elif file_type.clickedButton() == png_button:
+            plt.savefig(self.file_name + ".png")
+            subprocess.Popen(self.file_name + '.png', shell=True)
+        elif file_type.clickedButton() == cancel_button:
+            file_type.close()
