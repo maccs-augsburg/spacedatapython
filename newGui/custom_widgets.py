4ddf5f6a5d8e2076d12c8e7b5838037e3096738b
Mark Ortega <mortegaponce27@gmail.com>
2022-06-27 11:08:32 -0500

    Changed sizing

--- newGui/custom_widgets.py
+++ newGui/custom_widgets.py
@@ -92,7 +92,7 @@ class PushButton(QPushButton):
         self.button_is_checked = False
         self.clicked.connect(self.button_toggle)
         self.setChecked(self.button_is_checked)
-        self.setMaximumWidth(145)#145
+        self.setMaximumWidth(150)#145
         self.alternate_name = alternate_name
         self.original_name = label_name
         #self.setMaximumHeight(30)
