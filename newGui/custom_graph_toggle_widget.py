35214d818d25799681b2287e2b91a13b6d417242
Christopher J Hance <hance@augsburg.edu>
2022-07-07 11:49:53 -0500

    Just trying to toggle one button and grey out the other and such

--- newGui/custom_graph_toggle_widget.py
+++ newGui/custom_graph_toggle_widget.py
@@ -11,12 +11,12 @@ from PySide6.QtGui import  (
     QColor, QBrush, 
     QPaintEvent, QPen, 
     QPainter, QIcon, 
-    QAction)
+    QAction, QPalette)
 
 from PySide6.QtCore import (
         Qt, QSize, QPoint)
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtGui
 
 from custom_widgets import PushButton
 
@@ -27,12 +27,35 @@ class SwitchButtonWidget(QtWidgets.QWidget):
     """
     def __init__(self, *args,**kwargs):
         super(SwitchButtonWidget,self).__init__(*args,**kwargs)
-
+        button_layout = QtWidgets.QHBoxLayout()
         # Using our already created pushbutton widget to create our buttons here
         # We can still customize it as needed or scrap it and use our own for this widget
         self.three_axis_style = PushButton("Three Axis Style")
         self.stacked_axis_style = PushButton("Stacked Axis Style")
-    
+
+        button_layout.addWidget(self.three_axis_style)
+        button_layout.addWidget(self.stacked_axis_style)
+
+        # self.three_axis_style.setContentsMargins(0,0,0,0)
+        # self.stacked_axis_style.setContentsMargins(0,0,0,0)
+        # button_layout.setContentsMargins(0,0,0,0)
+
+        self.setLayout(button_layout)
+
+        self.three_axis_style.clicked.connect(self.button_toggle)
+        self.stacked_axis_style.clicked.connect(self.button_toggle)
+
+    #make same button toggle function for these buttons 
+    #and if one button is toggled we want to untoggle the other button
+    def button_toggle(self, checked):
+        self.button_is_checked = checked
+        if self.three_axis_style.isChecked():
+            self.three_axis_style.setPalette(QColor('black'))
+            self.stacked_axis_style.setPalette(QColor("grey"))
+        else:
+            self.three_axis_style.setPalette(QPalette.Inactive)
+
+
 def main():
     app = QApplication(sys.argv)
     window = SwitchButtonWidget()
