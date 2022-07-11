bc67e8070588b7825dfa95ad6a73abc79f6aaeb9
Christopher J Hance <hance@augsburg.edu>
2022-07-11 12:52:26 -0500

    removed uneeded button click function

--- newGui/custom_graph_toggle_widget.py
+++ newGui/custom_graph_toggle_widget.py
@@ -37,39 +37,18 @@ class SwitchButtonWidget(QtWidgets.QWidget):
         button_layout.addWidget(self.three_axis_style)
         button_layout.addWidget(self.stacked_axis_style)
 
-        # self.three_axis_style.setContentsMargins(0,0,0,0)
-        # self.stacked_axis_style.setContentsMargins(0,0,0,0)
-        # button_layout.setContentsMargins(0,0,0,0)
-
         self.setLayout(button_layout)
-
-        self.three_axis_style.clicked.connect(self.button_clicked)
-        self.stacked_axis_style.clicked.connect(self.button_clicked)
 
         self.three_axis_style.clicked.connect(self.three_axis_checked)
         self.stacked_axis_style.clicked.connect(self.stacked_axis_checked)
 
     def three_axis_checked(self):
-        print("Three axis clicked uncheck stacked ")
         self.stacked_axis_style.setChecked(False)
         self.three_axis_style.setChecked(True)
-        return True
         
     def stacked_axis_checked(self):
-        print("Stacked axis clicked uncheck Three axis ")
         self.three_axis_style.setChecked(False)
         self.stacked_axis_style.setChecked(True)
-        return False
-
-    #make same button toggle function for these buttons 
-    #and if one button is toggled we want to untoggle the other button
-    def button_clicked(self, checked):
-        color = QColor(Qt.cyan)
-        self.button_is_checked = checked
-        if self.three_axis_style.isChecked():
-            self.stacked_axis_style.setChecked(False)
-        elif self.stacked_axis_style.isChecked():
-            self.three_axis_style.setChecked(False)
 
 def main():
     app = QApplication(sys.argv)
