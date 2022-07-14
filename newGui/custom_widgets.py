222ace20f29f65b01533a7e181a81a56458b1751
Christopher J Hance <hance@augsburg.edu>
2022-07-14 14:54:09 -0500

    commented out some vars in spingbox class

--- newGui/custom_widgets.py
+++ newGui/custom_widgets.py
@@ -135,10 +135,10 @@ class Spinbox(QSpinBox):
         self.setWrapping(True)
 
         self.valueChanged.connect(self.value_changed)
-        self.setAlignment(Qt.AlignCenter)
+       # self.setAlignment(Qt.AlignCenter)
         self.entry = 0
         self.setMaximumWidth(95)
-        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+       # self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.setMaximumHeight(MINIMUM_HEIGHT)
 
     def value_changed(self, i):
