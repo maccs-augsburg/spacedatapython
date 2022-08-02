e296707e9942c99ae1d83e929b6f2c1c5a830eac
Mark Ortega <mortegaponce27@gmail.com>
2022-08-02 14:28:46 -0500

    Data removers for clean,raw,iaga2002, still need testing

--- time_data_remover.py
+++ time_data_remover.py
@@ -1,6 +1,8 @@
 import datetime
 from tarfile import _Bz2ReadableFileobj
 
+from anyio import current_effective_deadline
+
 def remove_data_from_clean(infile, outfile, start_time, end_time):
 
     '''
@@ -45,31 +47,44 @@ def remove_data_from_raw(infile, outfile, start_time, end_time):
             break
 
 def remove_data_from_iaga(infile, outfile, start_time, end_time):
-    pass
 
+    for i in range(0, 100):
+        dummy_record = str(infile.readline())
+        dummy_record = dummy_record.split()
 
-def print_contents( infile, stime, etime) :
-    """ Prints the given file to standard out beginning at the given time.
+        outfile.write(dummy_record)
 
-    Prints the records in the file object by reading them one at a time
-    beginning at the given time until the end of the file.
-
-    Parameters
-    ----------
-    infile :
-        The file object to be read.
-    stime :
-        The datetime.time object from which to begin
-    etime :
-        The datetime.time object at which to end
-    """
-
-    while True :
-        one_record = infile.read( 28)
-        if not one_record :
+        if dummy_record[0].__contains__("DATE"):
+            outfile.write(dummy_record)
             break
-        current_time = datetime.time(hour=one_record[1], minute=one_record[2], second=one_record[3])
-        if current_time >= stime :
-            print_one_record( one_record)
-        if current_time >= etime :
+
+    # Loop until the end of file or end time has been reached
+    while True:
+
+        one_record_first_line = infile.readline()
+        one_record_first_line = one_record_first_line.split()
+
+        one_record_second_line = infile.readline()
+        one_record_second_line = one_record_second_line.split()
+
+        # if we reach the end then we break the loop
+        if not one_record_first_line:
+            break
+        
+        time = str(one_record_first_line[1])
+        # Grab up to hh:mm:ss ignoring the microseconds
+        time = time[2:10]
+        datetime_object = datetime.datetime.strptime(time, "%H:%M:%S").time()
+        hour = int(datetime_object.strftime("%H"))
+        minute = int(datetime_object.strftime("%M"))
+        second = int(datetime_object.strftime("%S"))
+
+        #second = one_record[17:23] //second with microsecond
+        current_time = datetime.time(hour, minute, second) # getting the current time
+
+        if current_time >= start_time:
+            outfile.write(one_record_first_line)
+            outfile.write(one_record_second_line)
+
+        if current_time > end_time:
             break
