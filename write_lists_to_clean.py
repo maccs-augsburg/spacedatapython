c8354684238462faa86044552469dcdabe4442b8
Mark Ortega <mortegaponce27@gmail.com>
2022-08-18 16:03:43 -0500

    write_lists_to_clean: Commented some code

--- write_lists_to_clean.py
+++ write_lists_to_clean.py
@@ -4,7 +4,7 @@ import argparse
 import clean_to_screen
 import model.read_clean_to_lists
 import model.read_raw_to_lists
-import model.read_IAGA2002_to_lists
+import model.read_iaga2002_to_lists
 
 def create_clean_file_from_datetime_lists(x_list, y_list, z_list, time_list, outfile):
 
@@ -15,15 +15,15 @@ def create_clean_file_from_datetime_lists(x_list, y_list, z_list, time_list, out
 
     while record_counter < len(time_list):
         
+        # values from lists are / 1000, multiply by 1000 to write them out
         x_one = int(x_list[record_counter] * 1000)
         x_two = int(x_list[record_counter + 1] * 1000)
         y_one = int(y_list[record_counter] * 1000)
         y_two = int(y_list[record_counter + 1] * 1000)
         z_one = int(z_list[record_counter] * 1000)
         z_two = int(z_list[record_counter + 1] * 1000)
-        # can also get the century, month, day from datetime_object
-        # Example when creating record for iaga.
-        # Would use to name file
+
+        # get current time of first two records
         datetime_object = time_list[record_counter]
         time = datetime_object.time()
         hour = time.hour
@@ -35,18 +35,19 @@ def create_clean_file_from_datetime_lists(x_list, y_list, z_list, time_list, out
         flag = 1
         #flag = flag_list[flag_counter]
 
+        # write out hardcoded flag, write out time
         outfile.write(flag.to_bytes(1, byteorder='big', signed=False))
         outfile.write(hour.to_bytes(1, byteorder='big', signed=False))
         outfile.write(minute.to_bytes(1, byteorder='big', signed=False))
         outfile.write(second.to_bytes(1, byteorder='big', signed=False))
-        # values from lists are / 1000, multiply by 1000 to write them out
+        # write out values in the speficied format
         outfile.write(x_one.to_bytes(4, byteorder='big', signed=True))
         outfile.write(x_two.to_bytes(4, byteorder='big', signed=True))
         outfile.write(y_one.to_bytes(4, byteorder='big', signed=True))
         outfile.write(y_two.to_bytes(4, byteorder='big', signed=True))
         outfile.write(z_one.to_bytes(4, byteorder='big', signed=True))
         outfile.write(z_two.to_bytes(4, byteorder='big', signed=True))
-        # write a newline?
+        # write newline?
         record_counter += 2
         flag_counter += 1
 
