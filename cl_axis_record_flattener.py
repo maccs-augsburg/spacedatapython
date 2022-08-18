797814e94fcb7275b0fb61c14f94165fbe083f61
Mark Ortega <mortegaponce27@gmail.com>
2022-08-18 09:54:02 -0500

    cl_axis_flattener: Added updated function to flatten an axis

--- cl_axis_record_flattener.py
+++ cl_axis_record_flattener.py
@@ -26,7 +26,7 @@ import model.read_IAGA2002_to_lists
 import write_lists_to_iaga2002
 
 # https://matplotlib.org/stable/gallery/lines_bars_and_markers/masked_demo.html
-AXIS_FLATENNER_VALUE = "99999.99"
+AXIS_FLATTENER_VALUE = "99999.99"
 
 def flatten_axis_from_clean(infile, outfile, axis, start_time, end_time):
 
@@ -64,9 +64,9 @@ def flatten_axis_from_clean(infile, outfile, axis, start_time, end_time):
         if current_time >= start_time and current_time <= end_time:
             # read_clean_to_lists for example
             # https://stackoverflow.com/questions/6187699/how-to-convert-integer-value-to-array-of-four-bytes-in-python
-            outfile.write(AXIS_FLATENNER_VALUE.to_bytes(4, byteorder='big', signed=True))
+            outfile.write(AXIS_FLATTENER_VALUE.to_bytes(4, byteorder='big', signed=True))
             dummy_record = infile.read(4)
-            outfile.write(AXIS_FLATENNER_VALUE.to_bytes(4, byteorder='big', signed=True))
+            outfile.write(AXIS_FLATTENER_VALUE.to_bytes(4, byteorder='big', signed=True))
             dummy_record = infile.read(4)
             one_record = infile.read(28 - (second_tuple[1] + 1))
             outfile.write(one_record)
@@ -113,14 +113,14 @@ def flatten_axis_from_raw(infile, outfile, axis, start_time, end_time):
 
         if current_time >= start_time and current_time <= end_time:
             # write next three bytes (overwrite with new value)
-            outfile.write(model.raw_codecs.encode(AXIS_FLATENNER_VALUE))
+            outfile.write(model.raw_codecs.encode(AXIS_FLATTENER_VALUE))
             # move up 3 bytes, dont write out to file
             dummy_record = infile.read(3)
             # write out bytes in between first x-value and next
             one_record = infile.read(5)
             outfile.write(one_record)
             # repeat
-            outfile.write(model.raw_codecs.encode(AXIS_FLATENNER_VALUE))
+            outfile.write(model.raw_codecs.encode(AXIS_FLATTENER_VALUE))
             dummy_record = infile.read(3)
             # grab the rest of the records
             one_record = infile.read(38 - second_tuple[1])
@@ -130,95 +130,7 @@ def flatten_axis_from_raw(infile, outfile, axis, start_time, end_time):
             one_record = infile.read(38 - first_tuple[0])
             outfile.write(one_record)
 
-
-def flatten_axis_from_iaga(infile, outfile, axis, start_time, end_time):
-
-    axis = axis.lower()
-
-    if axis == "x":
-        index = 3
-    elif axis == "y":
-        index = 4
-    else:
-        index = 5
-    
-    for i in range(0, 100):
-
-        dummy_record = str(infile.readline())
-        dummy_record_list = dummy_record.split()
-
-        if dummy_record_list[0].__contains__("DATE"):
-            outfile.write(dummy_record)
-            break
-        
-        outfile.write(dummy_record)
-        
-    axis_value = f"{AXIS_FLATENNER_VALUE:10.2f}"
-    while True:
-
-        one_record_first_line = infile.readline()
-        one_record_second_line = infile.readline()
-        list_one = list(one_record_first_line.split())
-        list_two = list(one_record_second_line.split())
-
-        # if we reach the end then we break the loop
-        if not one_record_first_line:
-            break
-        # Grab the time hh:mm:ss:mm
-        time = list_one[1]
-        # Grab up to hh:mm:ss ignoring the microseconds
-        time = time[0:8]
-        datetime_object = datetime.datetime.strptime(time, "%H:%M:%S").time()
-        hour = int(datetime_object.strftime("%H"))
-        minute = int(datetime_object.strftime("%M"))
-        second = int(datetime_object.strftime("%S"))
-
-        #second = one_record[17:23] //second with microsecond
-        current_time = datetime.time(hour, minute, second) # getting the current time
-
-        list_one[index] = axis_value
-        list_two[index] = axis_value
-
-        # Build string to match format already present in file
-        x1 = f"{float(list_one[3]):10.2f}"
-        x2 = f"{float(list_two[3]):10.2f}"
-        y1 = f"{float(list_one[4]):10.2f}"
-        y2 = f"{float(list_two[4]):10.2f}"
-        z1 = f"{float(list_one[5]):10.2f}"
-        z2 = f"{float(list_two[5]):10.2f}"
-
-        if current_time >= start_time and current_time <= end_time:
-            first_half = list_one[0] + " " + list_one[1] + " " + list_one[2] + "   " + x1 + y1 + z1 + "  88888.88\n"
-            second_half = list_two[0] + " " + list_two[1] + " " + list_two[2] + "   " + x2 + y2 + z2 + "  88888.88\n"
-            one_record = first_half + second_half
-            outfile.write(one_record)
-
-        else:
-            outfile.write(one_record_first_line)
-            outfile.write(one_record_second_line)
-
-def flatten_axis(infile, outfile, axis, start_time, end_time):
-
-    extension = infile.split('.')[-1]
-    infile = open(infile, 'rb')
-
-    start = datetime.time.fromisoformat( "00:00:00")
-    end = datetime.time.fromisoformat("23:59:59")
-
-    if extension == 's2':
-        x,y,z,t,f = model.read_clean_to_lists.create_datetime_lists_from_clean(
-            infile, start, end             
-        )
-        
-    elif extension == '2hz':
-        x,y,z,t = model.read_raw_to_lists.create_datetime_lists_from_raw(
-            infile, start, end
-        )
-    
-    elif extension == 'sec':
-        x,y,z,t = model.read_IAGA2002_to_lists.create_datetime_lists_from_iaga(
-            infile, start, end
-        )
+def flatten_axis(x,y,z,t, axis, start_time, end_time, outfile, station):
 
     modify_axis = None
     if axis == 'x':
@@ -232,16 +144,25 @@ def flatten_axis(infile, outfile, axis, start_time, end_time):
     end_index = None
 
     for i in range (len(t)):
-        if start_time == t[i]:
+        # convert to time
+        current_time = t[i].time()
+        # remove microseconds
+        current_time = current_time.strftime("%H:%M:%S")
+        # can't compare unless both datetime.time. Make shorter?
+        current_time = datetime.time.fromisoformat(current_time)
+
+        if start_time == current_time and start_index is None:
             start_index = i
-        if end_time == t[i]:
+        if end_time == current_time and end_index is None:
             end_index = i
-            
+       
     # add one to end_index, because open on end range
     for i in range (start_index, end_index + 1):
         modify_axis[i] = 99999.99
 
-    write_lists_to_iaga2002.create_iaga2002_file_from_datetime_lists(x,y,z,t,"CH20097_flatten_test.2hz")
+    outfile = open(outfile, 'w')
+    write_lists_to_iaga2002.create_iaga2002_file_from_datetime_lists(x,y,z,t, outfile, station)
+    outfile.close()
 
 def main():
     
@@ -250,32 +171,43 @@ def main():
     parser.add_argument('stime', type=str, nargs='?',
         default="10:00:00", help="time to start flattening an axis")
     parser.add_argument('etime', type=str, nargs='?',
-        default="23:59:59", help="time to stop flattening an axis")
+        default="20:00:00", help="time to stop flattening an axis")
     parser.add_argument('axis', type=str, nargs='?',
         default='x', help="the axis to flatten (x, y, z)")
     args = parser.parse_args()
 
-    tuple_filename = args.filename.split(".")
-    # filepath without extension
-    filename_noext = tuple_filename[0]
-    filename_extension = tuple_filename[1]
+    extension = args.filename.split('.')[-1]
+    infile = args.filename
+    infile = open(infile, 'rb')
+    start = datetime.time.fromisoformat( "00:00:00")
+    end = datetime.time.fromisoformat("23:59:59")
 
-    new_filename = filename_noext + "_test_flatten_axis." + filename_extension
+    if extension == 's2':
+        x,y,z,t,f = model.read_clean_to_lists.create_datetime_lists_from_clean(
+            infile, start, end             
+        )
+        station_code = args.filename[0:2]
+        
+    elif extension == '2hz':
+        x,y,z,t = model.read_raw_to_lists.create_datetime_lists_from_raw(
+            infile, start, end
+        )
+        station_code = args.filename[0:2]
+    
+    elif extension == 'sec':
+        x,y,z,t = model.read_IAGA2002_to_lists.create_datetime_lists_from_iaga(
+            infile, start, end
+        )
+        station_code = args.filename[0:3]
+
+    infile.close()
+
     start_time = datetime.time.fromisoformat(args.stime)
     end_time = datetime.time.fromisoformat(args.etime)
 
-    if filename_extension == "s2":
-        infile = open(args.filename, "rb")
-        outfile = open(new_filename, "wb")
-        flatten_axis_from_clean(infile, outfile, args.axis, start_time, end_time)
-    if filename_extension == "2hz":
-        infile = open(args.filename, "rb")
-        outfile = open(new_filename, "wb")
-        flatten_axis_from_raw(infile, outfile, args.axis, start_time, end_time)
-    if filename_extension == "sec":
-        infile = open(args.filename, "r")
-        outfile = open(new_filename, "w")
-        flatten_axis_from_iaga(infile, outfile, args.axis, start_time, end_time)
+    outfile_name = args.filename.split('.')[0] + "_flatten_test.sec"
+
+    flatten_axis(x,y,z,t, "x", start_time, end_time, outfile_name, station_code)
 
 if __name__ == "__main__":
     main()
